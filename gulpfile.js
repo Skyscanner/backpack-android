56618605ede0ba4354ea04eece620228c0067192
George333123 <georgegillams@hotmail.co.uk>
2018-02-02 11:29:15 +0000

    [BPK-1261] Add eslint config to backpack-android

--- gulpfile.js
+++ gulpfile.js
@@ -17,55 +17,46 @@
  */
 
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const path = require('path');
 const nunjucks = require('gulp-nunjucks');
-const fs = require('fs');
 const rename = require('gulp-rename');
 const runSequence = require('run-sequence');
 const del = require('del');
 const _ = require('lodash');
-const tinycolor = require("tinycolor2");
 const tokens = require('bpk-tokens/tokens/base.raw.android.json');
 
-const __PATHS__ = {
-    templates: path.join(__dirname, 'templates'),
-    outputRes: path.join(__dirname, 'Backpack', 'src', 'main', 'res', 'values'),
-
+const PATHS = {
+  templates: path.join(__dirname, 'templates'),
+  outputRes: path.join(__dirname, 'Backpack', 'src', 'main', 'res', 'values'),
 };
 
+const pascalCase = s => _.flow(_.camelCase, _.upperFirst)(s);
 
-const pascalCase = (s) => _.flow(_.camelCase, _.upperFirst)(s);
+const getTokenType = type =>
+  Object.values(tokens.props).filter(i => i.type === type);
+const getColors = () =>
+  getTokenType('color').map(color => {
+    const newColor = JSON.parse(JSON.stringify(color));
+    newColor.name = `bpk${pascalCase(
+      newColor.name.replace(newColor.type.toUpperCase(), ''),
+    )}`;
+    return newColor;
+  });
 
+gulp.task('template:color', () =>
+  gulp
+    .src(`${PATHS.templates}/BackpackColor.njk`)
+    .pipe(
+      nunjucks.compile({
+        data: getColors(),
+      }),
+    )
+    .pipe(rename('backpack.color.xml'))
+    .pipe(gulp.dest(PATHS.outputRes)),
+);
 
-const parseColor = (c) => {
-    let str = tinycolor(c).toHex8().toUpperCase();
-    let alpha = str.substr(6);
-    return '#' + alpha + str.substr(0, 6);
-};
+gulp.task('default', () => {
+  runSequence('template:color');
+});
 
-const getTokenType = (type) => {
-    return Object.values(tokens.props)
-        .filter(i => i.type === type);
-}
-const getColors = () => {
-    return getTokenType("color").map(color => {
-        color.name = `bpk${pascalCase(color.name.replace(color.type.toUpperCase(), ""))}`;
-        return color;
-    });
-}
-
-gulp.task('template:color', () => (
-    gulp.src(__PATHS__.templates + '/BackpackColor.njk')
-        .pipe(nunjucks.compile({
-            'data': getColors()
-        }))
-        .pipe(rename('backpack.color.xml'))
-        .pipe(gulp.dest(__PATHS__.outputRes))
-));
-
- gulp.task('default', () => {
-     runSequence('template:color')
- });
-
- gulp.task('clean', () => del([__PATHS__.outputRes], { force: true }));
+gulp.task('clean', () => del([PATHS.outputRes], { force: true }));
