ec0dcfb96ef64bec4b162331e5b9e7d01d61f115
Matteo Hertel <info@matteohertel.com>
2018-02-09 16:55:39 +0000

    [BPK-1340] improve release process

--- app/src/main/java/net/skyscanner/backpack/MainActivity.kt
+++ app/src/main/java/net/skyscanner/backpack/MainActivity.kt
@@ -3,16 +3,16 @@ package net.skyscanner.backpack
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(R.layout.activity_main);
 
-  fun showMore(view: View){
-    Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show();
-  }
+        val btn = findViewById<Button>(R.id.toast_button) as Button;
+        btn.setOnClickListener {view -> Toast.makeText(this, "Coming Soon", Toast.LENGTH_SHORT).show()}
+    }
 }
