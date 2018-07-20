5cefe054c2ab9d8b6fe10ced8e1875e6fc17545e
Amol Gupta <amolgupta1989@gmail.com>
2018-07-20 13:08:54 +0100

    update emulator settings and scripts

---
+++ create-emulator.sh
@@ -0,0 +1,10 @@
+# Author : Amol Gupta
+# Date   : 20th July 2018
+# This file creates the emulator based on standard settings.
+# Use this emulator for all testing purposes including snapshot tests on CI.
+
+mksdcard -l e 1024M test-sd.img
+sdkmanager "system-images;android-21;default;armeabi-v7a"
+echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n bpk-avd  -k "system-images;android-21;default;armeabi-v7a"
+$ANDROID_HOME//emulator/emulator -avd bpk-avd -sdcard test-sd.img &
+cp bpk-avd.ini ~/.android/avd/bpk-avd.avd/config.ini &
