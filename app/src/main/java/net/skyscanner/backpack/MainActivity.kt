287057da605cb6706a1692f038b6ab30c65af9b0
Amol Gupta <amolgupta1989@gmail.com>
2018-08-15 13:24:52 +0100

    move bpk-badge

--- app/src/main/java/net/skyscanner/backpack/MainActivity.kt
+++ app/src/main/java/net/skyscanner/backpack/MainActivity.kt
@@ -1,8 +1,7 @@
 package net.skyscanner.backpack
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
 
