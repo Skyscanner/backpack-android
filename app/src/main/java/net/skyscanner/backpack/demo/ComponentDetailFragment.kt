5f563e28d28eabcc0079fa37f2ee322ad5428b58
Amol Gupta <amolgupta1989@gmail.com>
2018-08-24 16:57:36 +0100

    RTL testing support

--- app/src/main/java/net/skyscanner/backpack/demo/ComponentDetailFragment.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/ComponentDetailFragment.kt
@@ -1,8 +1,16 @@
 package net.skyscanner.backpack.demo
 
 import android.os.Bundle
+import android.support.annotation.LayoutRes
 import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.SwitchCompat
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
 import net.skyscanner.backpack.demo.data.ComponentRegistry
 
 /**
@@ -17,34 +25,45 @@ import net.skyscanner.backpack.demo.data.ComponentRegistry
  */
 open class ComponentDetailFragment : Fragment() {
 
+  /**
+   * The dummy content this fragment is presenting.
+   */
+  private var mItem: ComponentRegistry.Component? = null
+  var childView: LinearLayoutCompat? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (arguments!!.containsKey(ARG_ITEM_ID)) {
+      // Load the dummy content specified by the fragment
+      // arguments. In a real-world scenario, use a Loader
+      // to load content from a content provider.
+      mItem = ComponentRegistry.ITEM_MAP[arguments!!.getString(ARG_ITEM_ID)]
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val view: View = inflater.inflate(R.layout.fragment_detail, container, false)
+    val switch = view.findViewById<SwitchCompat>(R.id.toggle_rtl)
+    switch.setOnCheckedChangeListener({ _, isChecked ->
+      Log.i("switch_compat", "checked state is $isChecked")
+      childView?.layoutDirection = if (isChecked) View.LAYOUT_DIRECTION_RTL else View.LAYOUT_DIRECTION_LTR
+    })
+    return view
+  }
+
+  fun merge(@LayoutRes layout: Int) {
+
+    childView = view?.findViewById(R.id.story_container)
+    childView?.addView(layoutInflater.inflate(layout, childView, false))
+  }
+
+  companion object {
     /**
-     * The dummy content this fragment is presenting.
+     * The fragment argument representing the item ID that this fragment
+     * represents.
      */
-    private var mItem: ComponentRegistry.Component? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (arguments!!.containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = ComponentRegistry.ITEM_MAP[arguments!!.getString(ARG_ITEM_ID)]
-
-            val activity = this.activity
-            val appBarLayout = activity!!.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)
-            if (appBarLayout != null) {
-                appBarLayout.title = mItem!!.id
-            }
-        }
-    }
-
-    companion object {
-        /**
-         * The fragment argument representing the item ID that this fragment
-         * represents.
-         */
-        val ARG_ITEM_ID = "item_id"
-    }
+    val ARG_ITEM_ID = "item_id"
+  }
 
 }
