43a29fd5be6430f30552a4db4d464fc53e8ad4da
Amol Gupta <amolgupta1989@gmail.com>
2018-09-06 13:12:20 +0100

    update to accommodate app theme in snapshots

--- app/src/androidTest/java/net/skyscanner/backpack/gradient/BpkGradientsTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/gradient/BpkGradientsTest.kt
@@ -1,8 +1,6 @@
 package net.skyscanner.backpack.gradient
 
-import android.content.Context
 import android.graphics.drawable.GradientDrawable
-import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
 import net.skyscanner.backpack.BpkSnapshotTest
@@ -13,11 +11,9 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 
 class BpkGradientsTest: BpkSnapshotTest()  {
-  private lateinit var testContext: Context
 
   @Before
   fun setup() {
-    testContext = InstrumentationRegistry.getTargetContext()
     setDimensions(100,100)
   }
 
--- app/src/androidTest/java/net/skyscanner/backpack/text/BpkTextTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/text/BpkTextTest.kt
@@ -1,33 +1,26 @@
 package net.skyscanner.backpack.text
 
 
-import android.content.Context
-import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import com.facebook.testing.screenshot.Screenshot
-import com.facebook.testing.screenshot.ViewHelpers
+import net.skyscanner.backpack.BpkSnapshotTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 
-class BpkTextTest {
-  private lateinit var testContext: Context
+class BpkTextTest: BpkSnapshotTest()  {
 
   @Before
   fun setup() {
-    testContext = InstrumentationRegistry.getTargetContext()
+    setDimensions(32,96)
   }
 
   @Test
   fun screenshotTestTextDefault() {
     val text = BpkText(testContext)
     text.text = "Message"
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 
   @Test
@@ -35,10 +28,7 @@ class BpkTextTest {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.LG
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 
   @Test
@@ -46,43 +36,34 @@ class BpkTextTest {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.BASE
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextSm() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.SM
     text.emphasize = false
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextXl() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.XL
     text.emphasize = false
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextXs() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.XS
     text.emphasize = false
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 
   @Test
@@ -91,21 +72,16 @@ class BpkTextTest {
     text.text = "Message"
     text.textStyle = BpkText.XXL
     text.emphasize = false
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextXXLEmphasize() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.XXL
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 
   @Test
@@ -114,10 +90,7 @@ class BpkTextTest {
     text.text = "Message"
     text.textStyle = BpkText.LG
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 
   @Test
@@ -126,42 +99,33 @@ class BpkTextTest {
     text.text = "Message"
     text.textStyle = BpkText.BASE
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextSmEmphasize() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.SM
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextXlEmphasize() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.XL
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
+
   @Test
   fun screenshotTestTextXsEmphasize() {
     val text = BpkText(testContext)
     text.text = "Message"
     text.textStyle = BpkText.XS
     text.emphasize = true
-    ViewHelpers.setupView(text)
-      .layout()
-    Screenshot.snap(text)
-      .record()
+    snap(text)
   }
 }
