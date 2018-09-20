0e9b79cb2a81b4a768bacec8958d393e5ab8874c
Amol Gupta <amolgupta1989@gmail.com>
2018-09-20 16:23:22 +0100

    Bpk 1935 icon docs (#71)

--- app/src/main/java/net/skyscanner/backpack/demo/data/ComponentRegistry.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/data/ComponentRegistry.kt
@@ -2,6 +2,7 @@ package net.skyscanner.backpack.demo.data
 
 import net.skyscanner.backpack.demo.ComponentDetailFragment
 import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.demo.stories.IconsStory
 import net.skyscanner.backpack.demo.stories.Story
 
 /**
@@ -18,7 +19,9 @@ object ComponentRegistry {
 
   val TOKENS by lazy { TOKENS_MAP.keys.toList() }
   val TOKENS_MAP = mapOf(
-    "Radii" to { Story.of(R.layout.fragment_radii) }
+    "Radii" to { Story.of(R.layout.fragment_radii) },
+    "Icons" to { Story.of(R.layout.fragment_icons) },
+    "All Icons" to { IconsStory() }
   )
 
   val ALL by lazy {
