826a6aa942a5f61a4ed8b51778202c67ec57d1f9
Amol Gupta <amolgupta1989@gmail.com>
2018-09-21 14:45:53 +0100

    [No-JIRA] Add stetho inspector for layouts

--- app/src/main/java/net/skyscanner/backpack/demo/BackpackDemoApplication.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/BackpackDemoApplication.kt
@@ -2,6 +2,7 @@ package net.skyscanner.backpack.demo
 
 import android.support.multidex.MultiDexApplication
 import android.support.v7.app.AppCompatDelegate
+import com.facebook.stetho.Stetho
 
 /**
  * Application class registered in AndroidManifest.xml
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatDelegate
 class BackpackDemoApplication : MultiDexApplication() {
   override fun onCreate() {
     super.onCreate()
+    Stetho.initializeWithDefaults(this);
     AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
   }
 }
