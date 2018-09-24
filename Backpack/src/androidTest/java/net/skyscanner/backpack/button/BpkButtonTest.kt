3ba74165d11f830cc419bdfc83b78396159fc843
Amol Gupta <amolgupta1989@gmail.com>
2018-09-24 16:36:25 +0100

    mend

--- Backpack/src/androidTest/java/net/skyscanner/backpack/button/BpkButtonTest.kt
+++ Backpack/src/androidTest/java/net/skyscanner/backpack/button/BpkButtonTest.kt
@@ -2,7 +2,7 @@ package net.skyscanner.backpack.button
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import android.support.v4.content.ContextCompat
+import android.support.v7.content.res.AppCompatResources
 import net.skyscanner.backpack.R
 import org.junit.Assert
 import org.junit.Test
@@ -26,23 +26,17 @@ class BpkButtonTest {
   @Test
   fun test_icon_end() {
     val context = InstrumentationRegistry.getInstrumentation().targetContext
-    val trainIcon = ContextCompat.getDrawable(context, R.drawable.bpk_train)
+    val trainIcon = AppCompatResources.getDrawable(context, R.drawable.bpk_train)
     val button = BpkButton(context).apply {
       iconEnd = trainIcon
     }
     Assert.assertEquals(button.compoundDrawables[2],trainIcon)
-    BpkButton(context).apply {
-      iconEnd = ContextCompat.getDrawable(context, R.drawable.bpk_weather)
-      iconStart = ContextCompat.getDrawable(context, R.drawable.bpk_weather)
-      type = BpkButton.Type.Primary
-      text = "Message"
-    }
   }
 
   @Test
   fun test_icon_start() {
     val context = InstrumentationRegistry.getInstrumentation().targetContext
-    val trainIcon = ContextCompat.getDrawable(context, R.drawable.bpk_train)
+    val trainIcon = AppCompatResources.getDrawable(context, R.drawable.bpk_train)
     val button = BpkButton(context).apply {
       iconStart = trainIcon
     }
