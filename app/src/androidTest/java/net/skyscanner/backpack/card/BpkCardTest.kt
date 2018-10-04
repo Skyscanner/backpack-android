d4f892d43911387583861062f8e9c32c57f1b025
Amol Gupta <amolgupta1989@gmail.com>
2018-10-04 15:03:59 +0200

    Adding test cases

---
+++ app/src/androidTest/java/net/skyscanner/backpack/card/BpkCardTest.kt
@@ -0,0 +1,70 @@
+package net.skyscanner.backpack.card
+
+
+import android.support.test.runner.AndroidJUnit4
+import android.widget.TextView
+import net.skyscanner.backpack.BpkSnapshotTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+
+class BpkCardTest : BpkSnapshotTest() {
+
+  @Before
+  fun setup() {
+    setDimensions(150, 150)
+  }
+
+  @Test
+  fun screenshotTestCardDefault() {
+    val card = BpkCard(testContext)
+    val text = TextView(testContext)
+    text.text = "message"
+    card.addView(text)
+    snap(card)
+  }
+
+  @Test
+  fun screenshotTestCardWithPadding() {
+    val card = BpkCard(testContext)
+    val text = TextView(testContext)
+    text.text = "message"
+    card.addView(text)
+    card.padded = true
+    snap(card)
+  }
+
+  @Test
+  fun screenshotTestCardWithoutPadding() {
+    val card = BpkCard(testContext)
+    val text = TextView(testContext)
+    text.text = "message"
+    card.addView(text)
+    card.padded = false
+    snap(card)
+  }
+
+
+  @Test
+  fun screenshotTestCardWithFocus() {
+    val card = BpkCard(testContext)
+    val text = TextView(testContext)
+    text.text = "message"
+    card.addView(text)
+    card.focused = true
+    snap(card)
+  }
+
+  @Test
+  fun screenshotTestCardWithoutPaddingAndFocus() {
+    val card = BpkCard(testContext)
+    val text = TextView(testContext)
+    text.text = "message"
+    card.addView(text)
+    card.padded = true
+    card.focused = true
+    snap(card)
+  }
+}
