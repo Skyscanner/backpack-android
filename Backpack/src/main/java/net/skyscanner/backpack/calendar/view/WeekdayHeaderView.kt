714b33c8f2e6c518a8779fe26c1cf8c7a1e0d269
Antal Monori <antal.monori@skyscanner.net>
2018-12-17 13:32:49 +0100

    Fixed constraint layout on calendar
    Signed-off-by: Antal Monori <antal.monori@skyscanner.net>

--- Backpack/src/main/java/net/skyscanner/backpack/calendar/view/WeekdayHeaderView.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/calendar/view/WeekdayHeaderView.kt
@@ -2,12 +2,16 @@ package net.skyscanner.backpack.calendar.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import net.skyscanner.backpack.R
 import net.skyscanner.backpack.text.BpkText
+import net.skyscanner.backpack.util.ResourcesUtil
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,110 +19,134 @@ internal class WeekdayHeaderView @JvmOverloads constructor(
   context: Context,
   attrs: AttributeSet? = null,
   defStyle: Int = 0
-) : FrameLayout(context, attrs, defStyle) {
+) : LinearLayout(context, attrs, defStyle) {
 
   private val headerLabelTextColor: Int by lazy { ContextCompat.getColor(context, R.color.bpkGray500) }
+  private val separatorLineColor: Int by lazy { ContextCompat.getColor(context, R.color.bpkGray100) }
+  private val baseSpacing: Int by lazy { context.resources.getDimensionPixelSize(R.dimen.bpkSpacingBase) }
 
-  private val firstWeekdayHeaderView: BpkText by lazy {
+  private val firstWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.firstWeekdayHeaderLabel
+      id = R.id.firstWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val secondWeekdayHeaderView: BpkText by lazy {
+  private val secondWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.secondWeekdayHeaderLabel
+      id = R.id.secondWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val thirdWeekdayHeaderView: BpkText by lazy {
+  private val thirdWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.thirdWeekdayHeaderLabel
+      id = R.id.thirdWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val fourthWeekdayHeaderView: BpkText by lazy {
+  private val fourthWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.fourthWeekdayHeaderLabel
+      id = R.id.fourthWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val fifthWeekdayHeaderView: BpkText by lazy {
+  private val fifthWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.fifthWeekdayHeaderLabel
+      id = R.id.fifthWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val sixthWeekdayHeaderView: BpkText by lazy {
+  private val sixthWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.sixthWeekdayHeaderLabel
+      id = R.id.sixthWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
-  private val seventhWeekdayHeaderView: BpkText by lazy {
+  private val seventhWeekdayView: BpkText by lazy {
     BpkText(context).apply {
-      id = R.id.seventhWeekdayHeaderLabel
+      id = R.id.seventhWeekdayLabel
       setTextColor(headerLabelTextColor)
     }
   }
 
   init {
+    orientation = VERTICAL
 
+    initWeekdayLabelViews()
+  }
+
+  private fun initWeekdayLabelViews() {
     val containerLayout = ConstraintLayout(context)
+      .also {
+        it.layoutParams =
+          ConstraintLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT).apply {
+            marginEnd = baseSpacing
+            marginStart = baseSpacing
+          }
+      }
       .apply {
-        addView(firstWeekdayHeaderView)
-        addView(secondWeekdayHeaderView)
-        addView(thirdWeekdayHeaderView)
-        addView(fourthWeekdayHeaderView)
-        addView(fifthWeekdayHeaderView)
-        addView(sixthWeekdayHeaderView)
-        addView(seventhWeekdayHeaderView)
-      }.also { addView(it) }
+        addView(firstWeekdayView)
+        addView(secondWeekdayView)
+        addView(thirdWeekdayView)
+        addView(fourthWeekdayView)
+        addView(fifthWeekdayView)
+        addView(sixthWeekdayView)
+        addView(seventhWeekdayView)
+      }.also {
+        addView(it)
+      }
 
     val constraintSet = ConstraintSet()
     constraintSet.clone(containerLayout)
     constraintSet.createHorizontalChain(
       ConstraintSet.PARENT_ID, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
       intArrayOf(
-        firstWeekdayHeaderView.id, secondWeekdayHeaderView.id, thirdWeekdayHeaderView.id,
-        fourthWeekdayHeaderView.id, fifthWeekdayHeaderView.id, sixthWeekdayHeaderView.id,
-        seventhWeekdayHeaderView.id
+        firstWeekdayView.id, secondWeekdayView.id, thirdWeekdayView.id, fourthWeekdayView.id, fifthWeekdayView.id,
+        sixthWeekdayView.id, seventhWeekdayView.id
       ),
       null, ConstraintSet.CHAIN_SPREAD_INSIDE
     )
 
-    constraintSet.connect(firstWeekdayHeaderView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-    constraintSet.connect(firstWeekdayHeaderView.id, ConstraintSet.END, secondWeekdayHeaderView.id, ConstraintSet.START)
+    constraintSet.connect(firstWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(firstWeekdayView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
+    constraintSet.connect(firstWeekdayView.id, ConstraintSet.END, secondWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(firstWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(secondWeekdayHeaderView.id, ConstraintSet.START, firstWeekdayHeaderView.id, ConstraintSet.END)
-    constraintSet.connect(secondWeekdayHeaderView.id, ConstraintSet.END, thirdWeekdayHeaderView.id, ConstraintSet.START)
+    constraintSet.connect(secondWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(secondWeekdayView.id, ConstraintSet.START, firstWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(secondWeekdayView.id, ConstraintSet.END, thirdWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(secondWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(thirdWeekdayHeaderView.id, ConstraintSet.START, secondWeekdayHeaderView.id, ConstraintSet.END)
-    constraintSet.connect(thirdWeekdayHeaderView.id, ConstraintSet.END, fourthWeekdayHeaderView.id, ConstraintSet.START)
+    constraintSet.connect(thirdWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(thirdWeekdayView.id, ConstraintSet.START, secondWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(thirdWeekdayView.id, ConstraintSet.END, fourthWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(thirdWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(fourthWeekdayHeaderView.id, ConstraintSet.START, thirdWeekdayHeaderView.id, ConstraintSet.END)
-    constraintSet.connect(fourthWeekdayHeaderView.id, ConstraintSet.END, fifthWeekdayHeaderView.id, ConstraintSet.START)
+    constraintSet.connect(fourthWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(fourthWeekdayView.id, ConstraintSet.START, thirdWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(fourthWeekdayView.id, ConstraintSet.END, fifthWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(fourthWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(fifthWeekdayHeaderView.id, ConstraintSet.START, fourthWeekdayHeaderView.id, ConstraintSet.END)
-    constraintSet.connect(fifthWeekdayHeaderView.id, ConstraintSet.END, sixthWeekdayHeaderView.id, ConstraintSet.START)
+    constraintSet.connect(fifthWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(fifthWeekdayView.id, ConstraintSet.START, fourthWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(fifthWeekdayView.id, ConstraintSet.END, sixthWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(fifthWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(sixthWeekdayHeaderView.id, ConstraintSet.START, fifthWeekdayHeaderView.id, ConstraintSet.END)
-    constraintSet.connect(
-      sixthWeekdayHeaderView.id,
-      ConstraintSet.END,
-      seventhWeekdayHeaderView.id,
-      ConstraintSet.START
-    )
+    constraintSet.connect(sixthWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(sixthWeekdayView.id, ConstraintSet.START, fifthWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(sixthWeekdayView.id, ConstraintSet.END, seventhWeekdayView.id, ConstraintSet.START)
+    constraintSet.connect(sixthWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
 
-    constraintSet.connect(
-      seventhWeekdayHeaderView.id,
-      ConstraintSet.START,
-      sixthWeekdayHeaderView.id,
-      ConstraintSet.START
-    )
-    constraintSet.connect(seventhWeekdayHeaderView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
+    constraintSet.connect(seventhWeekdayView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, baseSpacing)
+    constraintSet.connect(seventhWeekdayView.id, ConstraintSet.START, sixthWeekdayView.id, ConstraintSet.END)
+    constraintSet.connect(seventhWeekdayView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
+    constraintSet.connect(seventhWeekdayView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, baseSpacing)
+
     constraintSet.applyTo(containerLayout)
 
+    addView(
+      View(context).also { it.setBackgroundColor(separatorLineColor) },
+      LayoutParams(LayoutParams.MATCH_PARENT, ResourcesUtil.dpToPx(1, context))
+    )
   }
 
   internal fun initializeWithLocale(locale: Locale) {
@@ -133,13 +161,13 @@ internal class WeekdayHeaderView @JvmOverloads constructor(
       val calendarDayName = simpleDateFormat.format(calendar.time)
 
       when (i) {
-        0 -> firstWeekdayHeaderView.text = calendarDayName
-        1 -> secondWeekdayHeaderView.text = calendarDayName
-        2 -> thirdWeekdayHeaderView.text = calendarDayName
-        3 -> fourthWeekdayHeaderView.text = calendarDayName
-        4 -> fifthWeekdayHeaderView.text = calendarDayName
-        5 -> sixthWeekdayHeaderView.text = calendarDayName
-        6 -> seventhWeekdayHeaderView.text = calendarDayName
+        0 -> firstWeekdayView.text = calendarDayName
+        1 -> secondWeekdayView.text = calendarDayName
+        2 -> thirdWeekdayView.text = calendarDayName
+        3 -> fourthWeekdayView.text = calendarDayName
+        4 -> fifthWeekdayView.text = calendarDayName
+        5 -> sixthWeekdayView.text = calendarDayName
+        6 -> seventhWeekdayView.text = calendarDayName
       }
     }
   }
