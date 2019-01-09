a384db0d920f4b6f654796630060c5d925f500ec
Amol Gupta <amolgupta1989@gmail.com>
2019-01-09 15:08:04 +0000

    Support for marking extended classes

--- Backpack/src/main/java/net/skyscanner/backpack/util/BpkInterceptor.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/util/BpkInterceptor.kt
@@ -13,13 +13,25 @@ class BpkInterceptor(private val enable: Boolean = false) : Interceptor {
     val result = chain.proceed(chain.request())
     val view = result.view()
 
-    if (enable && view != null && view.javaClass.name.contains("Bpk")) {
+    if (enable && view != null && isBpkView(view.javaClass)) {
       if (view.background == null) {
+
         view.background = ColorDrawable(0x77ac650c)
       } else {
         view.foreground = ColorDrawable(0x77ac650c)
       }
     }
     return result
+  }
+
+  private fun isBpkView(javaClass: Class<Any>): Boolean {
+    var inClass = javaClass
+    while (inClass.superclass != null) {
+      if (inClass.name.toLowerCase().contains("bpk")) {
+        return true
+      }
+      inClass = (inClass.superclass as Class<Any>?)!!
+    }
+    return false
   }
 }
