2575b3bd594dc786be5f57cb44a153504bca6505
George Gillams <george.gillams@skyscanner.net>
2019-01-28 12:36:14 +0000

    Simple ls in script

--- upload-build.sh
+++ upload-build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -F ipa=@app/build/outputs/apk/release/app-release-unsigned.apk -H X-HockeyAppToken:43da894438f64ef08f5d35e5d037a379 https://rink.hockeyapp.net/api/2/apps/upload
+
+ls -la app/build/outputs/apk/release/app-release-unsigned.apk
+# curl -F ipa=@app/build/outputs/apk/release/app-release-unsigned.apk -H X-HockeyAppToken:43da894438f64ef08f5d35e5d037a379 https://rink.hockeyapp.net/api/2/apps/upload
