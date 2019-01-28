d2ba0512a9d0937460c83dd06c667710dba20c81
Amol Gupta <amolgupta1989@gmail.com>
2019-01-28 13:10:47 +0000

    one last try

--- upload-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ls -la app/build/outputs/apk/release/app-release-unsigned.apk
-# curl -F ipa=@app/build/outputs/apk/release/app-release-unsigned.apk -H X-HockeyAppToken:43da894438f64ef08f5d35e5d037a379 https://rink.hockeyapp.net/api/2/apps/upload
