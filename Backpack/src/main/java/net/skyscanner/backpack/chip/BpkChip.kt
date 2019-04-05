416768c80748416c6b2f240e1833531cd7e49a90
Tiago Henrique Engel <tiagohngl@gmail.com>
2019-04-05 11:12:34 +0100

    [BPK-2333] Add theme to chip (#234)

--- Backpack/src/main/java/net/skyscanner/backpack/chip/BpkChip.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/chip/BpkChip.kt
@@ -2,20 +2,22 @@ package net.skyscanner.backpack.chip
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.ContextThemeWrapper
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
-import androidx.core.widget.TextViewCompat
 import net.skyscanner.backpack.R
 import net.skyscanner.backpack.text.BpkText
+import net.skyscanner.backpack.util.createContextThemeOverlayWrapper
 
 open class BpkChip @JvmOverloads constructor(
   context: Context,
   attrs: AttributeSet? = null,
-  defStyleAttr: Int = R.style.Bpk_chip
-) : BpkText(context, attrs, defStyleAttr) {
+  defStyleAttr: Int = 0
+) : BpkText(createContextThemeOverlayWrapper(context, attrs), attrs, defStyleAttr) {
 
   init {
-    initialize(context, attrs, defStyleAttr)
+    initialize(attrs, defStyleAttr)
   }
 
   var disabled: Boolean = false
@@ -24,7 +26,7 @@ open class BpkChip @JvmOverloads constructor(
       this.isEnabled = !disabled
     }
 
-  private fun initialize(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+  private fun initialize(attrs: AttributeSet?, defStyleAttr: Int) {
     val attr = context.theme.obtainStyledAttributes(attrs, R.styleable.BpkChip, defStyleAttr, 0)
     disabled = attr.getBoolean(R.styleable.BpkChip_disabled, false)
     isSelected = attr.getBoolean(R.styleable.BpkChip_selected, false)
@@ -36,10 +38,18 @@ open class BpkChip @JvmOverloads constructor(
     // Elevation
     ViewCompat.setElevation(this, resources.getDimension(R.dimen.bpkElevationSm))
 
-    // Text
-    TextViewCompat.setTextAppearance(this, R.style.bpkTextSm)
+    textStyle = BpkText.SM
     this.setTextColor(ResourcesCompat.getColorStateList(resources, R.color.bpk_chip_text_color, context.theme))
     this.setSingleLine(true)
+
+    // Wrap with the default style so we always have a valid attribute in the xml
+    var context = ContextThemeWrapper(this.context, R.style.Bpk_chip)
+
+    val t = TypedValue()
+    if (context.theme.resolveAttribute(R.attr.bpkChipStyle, t, true)) {
+      // If we have global styles (theming) for the chip we wrap again so this overrides the default
+      context = ContextThemeWrapper(context, t.resourceId)
+    }
 
     // Background
     val drawable = ResourcesCompat.getDrawable(resources, R.drawable.chip_background, context.theme)
