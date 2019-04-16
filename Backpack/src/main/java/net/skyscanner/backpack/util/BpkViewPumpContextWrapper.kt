747f300bcde419870a27818ccd0dea60fb7c6532
Amol Gupta <amolgupta1989@gmail.com>
2019-04-16 17:11:54 +0100

    [BPK-2390] Disable ViewPump on Android Q

--- Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
@@ -1,18 +1,19 @@
+@file:JvmName("BpkViewPumpContextWrapper")
+
 package net.skyscanner.backpack.util
 
 import android.content.Context
+import android.content.ContextWrapper
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
-object BpkViewPumpContextWrapper {
-  /**
-   * Disables Viewpump on Android Q and above.
-   * See https://github.com/InflationX/ViewPump/issues/21
-   */
-  fun wrap(context: Context): Context {
-    return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.P) {
-      context
-    } else {
-      ViewPumpContextWrapper.wrap(context)
+class BpkViewPumpContextWrapper {
+  companion object {
+    fun wrap(context: Context): ContextWrapper {
+      return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.P) {
+        ContextWrapper(context)
+      } else {
+        ViewPumpContextWrapper.wrap(context)
+      }
     }
   }
 }
