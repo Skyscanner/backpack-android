24efc14ca0b7f93b35ccaa52763889955f0d488c
Amol Gupta <amolgupta1989@gmail.com>
2019-04-17 10:24:33 +0100

    Review comments

--- Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
@@ -5,11 +5,15 @@ import android.content.ContextWrapper
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 object BpkViewPumpContextWrapper {
-    fun wrap(context: Context): ContextWrapper {
-      return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.P) {
-        ContextWrapper(context)
-      } else {
-        ViewPumpContextWrapper.wrap(context)
-      }
+  /**
+   * Disables Viewpump on Android Q and above.
+   * See https://github.com/InflationX/ViewPump/issues/21
+   */
+  fun wrap(context: Context): Context {
+    return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.P) {
+      ContextWrapper(context)
+    } else {
+      ViewPumpContextWrapper.wrap(context)
+    }
   }
 }
