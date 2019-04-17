b8d9d0b8e635fc758af8fb0138e72f3849bbfc51
Amol Gupta <amolgupta1989@gmail.com>
2019-04-17 10:40:43 +0100

    review comments

--- Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/util/BpkViewPumpContextWrapper.kt
@@ -1,7 +1,6 @@
 package net.skyscanner.backpack.util
 
 import android.content.Context
-import android.content.ContextWrapper
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 object BpkViewPumpContextWrapper {
@@ -11,7 +10,7 @@ object BpkViewPumpContextWrapper {
    */
   fun wrap(context: Context): Context {
     return if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.P) {
-      ContextWrapper(context)
+      context
     } else {
       ViewPumpContextWrapper.wrap(context)
     }
