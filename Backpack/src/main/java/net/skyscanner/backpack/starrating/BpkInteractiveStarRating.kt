c18fdc40b7adfe40489bddf0c1ac1944e66cada3
bvitaliyg <bvitaliyg@gmail.com>
2019-06-21 18:24:01 +0100

    [BPK-2583] Implemented interactive version.

---
+++ Backpack/src/main/java/net/skyscanner/backpack/starrating/BpkInteractiveStarRating.kt
@@ -0,0 +1,40 @@
+package net.skyscanner.backpack.starrating
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import net.skyscanner.backpack.R
+import net.skyscanner.backpack.starrating.internal.BpkStarRatingBase
+import net.skyscanner.backpack.util.createContextThemeWrapper
+
+open class BpkInteractiveStarRating @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
+) : BpkStarRatingBase(
+  context = createContextThemeWrapper(context, attrs, R.attr.bpkStarRatingStyle),
+  attrs = attrs,
+  defStyleAttr = defStyleAttr,
+  empty = R.drawable.bpk_star,
+  half = R.drawable.bpk_star_half,
+  full = R.drawable.bpk_star,
+  starSize = context.resources.getDimensionPixelSize(R.dimen.bpkSpacingXl)
+) {
+
+  var onRatingChangedListener: ((Float, Int) -> Unit)? = null
+
+  override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+    val x = if (layoutDirection == View.LAYOUT_DIRECTION_RTL) width - ev.x else ev.x
+    val itemWidth = width / maxRating
+    val selectedItems = x / itemWidth
+    rating = Math.round(selectedItems + 0.5f).toFloat()
+    onRatingChangedListener?.invoke(rating, maxRating)
+    return true
+  }
+
+  interface OnRatingChangedListener : (Float, Int) -> Unit {
+
+    override fun invoke(current: Float, max: Int)
+  }
+}
