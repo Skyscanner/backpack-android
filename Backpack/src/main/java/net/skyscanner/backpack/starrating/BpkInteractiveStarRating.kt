115fcf981b527b48ef47cd1495c22d90a0c5b2f8
bvitaliyg <bvitaliyg@gmail.com>
2019-06-25 15:41:44 +0100

    [BPK-2583] Updated the icons.

--- Backpack/src/main/java/net/skyscanner/backpack/starrating/BpkInteractiveStarRating.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/starrating/BpkInteractiveStarRating.kt
@@ -16,8 +16,8 @@ open class BpkInteractiveStarRating @JvmOverloads constructor(
   context = createContextThemeWrapper(context, attrs, R.attr.bpkInteractiveStarRatingStyle),
   attrs = attrs,
   defStyleAttr = defStyleAttr,
-  empty = R.drawable.bpk_star,
-  half = R.drawable.bpk_star_half,
+  empty = R.drawable.bpk_star_outline,
+  half = R.drawable.bpk_star,
   full = R.drawable.bpk_star,
   starSize = context.resources.getDimensionPixelSize(R.dimen.bpkSpacingXxl)
 ) {
