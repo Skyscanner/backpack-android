30ff2fa4998d31cdbd1e95afa72b34256de22000
bvitaliyg <bvitaliyg@gmail.com>
2019-07-03 14:34:18 +0100

    [BPK-2422] Updated the tests.

--- app/src/androidTest/java/net/skyscanner/backpack/horizontalnav/HorizontalNavTests.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/horizontalnav/HorizontalNavTests.kt
@@ -6,6 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.tabs.TabLayout
 import net.skyscanner.backpack.BpkSnapshotTest
 import net.skyscanner.backpack.createThemedContext
+import net.skyscanner.backpack.demo.R
 import net.skyscanner.backpack.horisontalnav.BpkHorizontalNav
 import org.junit.Before
 import org.junit.Test
@@ -18,37 +19,11 @@ class HorizontalNavTests : BpkSnapshotTest() {
 
   @Before
   fun setup() {
-    setDimensions(56, 300)
+    setDimensions(36, 300)
   }
 
   @Test
   fun screenshotHorizontalNav_Default() {
-    snap(horizontalNav)
-  }
-
-  @Test
-  fun screenshotHorizontalNav_scrollable() {
-    horizontalNav.tabMode = TabLayout.MODE_SCROLLABLE
-    snap(horizontalNav)
-  }
-
-  @Test
-  fun screenshotHorizontalNav_fixed() {
-    horizontalNav.tabMode = TabLayout.MODE_FIXED
-    snap(horizontalNav)
-  }
-
-  @Test
-  fun screenshotHorizontalNav_scrollableRtl() {
-    horizontalNav.layoutDirection = View.LAYOUT_DIRECTION_RTL
-    horizontalNav.tabMode = TabLayout.MODE_SCROLLABLE
-    snap(horizontalNav)
-  }
-
-  @Test
-  fun screenshotHorizontalNav_fixedRtl() {
-    horizontalNav.layoutDirection = View.LAYOUT_DIRECTION_RTL
-    horizontalNav.tabMode = TabLayout.MODE_FIXED
     snap(horizontalNav)
   }
 
@@ -59,9 +34,35 @@ class HorizontalNavTests : BpkSnapshotTest() {
   }
 
   @Test
+  fun screenshotHorizontalNav_fixed() {
+    horizontalNav.tabMode = TabLayout.MODE_FIXED
+    snap(horizontalNav)
+  }
+
+  @Test
+  fun screenshotHorizontalNav_scrollable() {
+    horizontalNav.tabMode = TabLayout.MODE_SCROLLABLE
+    snap(horizontalNav)
+  }
+
+  @Test
+  fun screenshotHorizontalNav_small() {
+    val horizontalNav = BpkHorizontalNav(testContext).init()
+    horizontalNav.size = BpkHorizontalNav.Size.Small
+    snap(horizontalNav)
+  }
+
+  @Test
   fun screenshotHorizontalNav_alternate() {
     val horizontalNav = BpkHorizontalNav(testContext).init()
+    horizontalNav.setBackgroundResource(R.color.bpkBlue600)
     horizontalNav.appearance = BpkHorizontalNav.Appearance.Alternate
+    snap(horizontalNav)
+  }
+
+  @Test
+  fun screenshotHorizontalNav_Rtl() {
+    horizontalNav.layoutDirection = View.LAYOUT_DIRECTION_RTL
     snap(horizontalNav)
   }
 
