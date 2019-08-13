edab0e070826912274614458c880b95190e768ee
bvitaliyg <bvitaliyg@gmail.com>
2019-08-13 17:54:56 +0100

    [BPK-2795] Implemented the Snackbar and added the examples.

---
+++ Backpack/src/main/java/net/skyscanner/backpack/snackbar/BpkSnackbar.kt
@@ -0,0 +1,166 @@
+package net.skyscanner.backpack.snackbar
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.ColorStateList
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.util.TypedValue
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import net.skyscanner.backpack.R
+import net.skyscanner.backpack.text.BpkFontSpan
+import net.skyscanner.backpack.text.BpkText
+import net.skyscanner.backpack.util.BpkTheme
+import net.skyscanner.backpack.util.use
+
+open class BpkSnackbar private constructor(
+  private val snackbar: Snackbar,
+  @ColorInt private val textColor: Int,
+  @ColorInt private val actionColor: Int,
+  @ColorInt private val backgroundColor: Int
+) {
+
+  private val textFontSpan = BpkFontSpan(snackbar.context, BpkText.SM, BpkText.Weight.NORMAL)
+  private val textColorSpan = ForegroundColorSpan(textColor)
+
+  private val actionFontSpan = BpkFontSpan(snackbar.context, BpkText.SM, BpkText.Weight.EMPHASIZED)
+  private val actionColorSpan = ForegroundColorSpan(actionColor)
+
+  init {
+    snackbar.view.backgroundTintList = ColorStateList.valueOf(backgroundColor)
+    snackbar.setActionTextColor(actionColor)
+  }
+
+  fun setText(message: CharSequence): BpkSnackbar {
+    val spanned = SpannableStringBuilder(message).apply {
+      setSpan(textFontSpan, 0, length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+      setSpan(textColorSpan, 0, length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+    }
+
+    snackbar.setText(spanned)
+    return this
+  }
+
+  fun setText(@StringRes resId: Int): BpkSnackbar =
+    setText(snackbar.context.getString(resId))
+
+  fun setAction(text: CharSequence, listener: View.OnClickListener): BpkSnackbar {
+    val spanned = SpannableStringBuilder(text).apply {
+      setSpan(actionFontSpan, 0, length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+      setSpan(actionColorSpan, 0, length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+    }
+
+    snackbar.setAction(spanned, listener)
+    return this
+  }
+
+  fun setAction(@StringRes resId: Int, listener: View.OnClickListener): BpkSnackbar =
+    setAction(snackbar.context.getText(resId), listener)
+
+  inline fun setAction(text: CharSequence, crossinline listener: (View) -> Unit): BpkSnackbar =
+    setAction(text, View.OnClickListener { listener(it) })
+
+  inline fun setAction(@StringRes resId: Int, crossinline listener: (View) -> Unit): BpkSnackbar =
+    setAction(resId, View.OnClickListener { listener(it) })
+
+  fun show(): Unit =
+    snackbar.show()
+
+  fun dismiss(): Unit =
+    snackbar.dismiss()
+
+  fun isShown(): Boolean =
+    snackbar.isShown
+
+  val duration: Int
+    get() = snackbar.duration
+
+  fun setDuration(duration: Int): BpkSnackbar {
+    snackbar.duration = duration
+    return this
+  }
+
+  @Deprecated("")
+  fun setCallback(callback: Snackbar.Callback): BpkSnackbar {
+    snackbar.setCallback(callback)
+    return this
+  }
+
+  fun setBehavior(behavior: BaseTransientBottomBar.Behavior): BpkSnackbar {
+    snackbar.behavior = behavior
+    return this
+  }
+
+   val behavior: BaseTransientBottomBar.Behavior?
+    get() = snackbar.behavior
+
+  val context: Context
+    get() = snackbar.context
+
+  val view: View
+    get() = snackbar.view
+
+  fun addCallback(callback: BaseTransientBottomBar.BaseCallback<Snackbar>): BpkSnackbar {
+    snackbar.addCallback(callback)
+    return this
+  }
+
+  fun removeCallback(callback: BaseTransientBottomBar.BaseCallback<Snackbar>): BpkSnackbar {
+    snackbar.removeCallback(callback)
+    return this
+  }
+
+  fun isShownOrQueued(): Boolean =
+    snackbar.isShownOrQueued
+
+  val rawSnackbar: Snackbar
+    get() = snackbar
+
+  companion object {
+
+    @JvmStatic
+    val LENGTH_INDEFINITE = Snackbar.LENGTH_INDEFINITE
+
+    @JvmStatic
+    val LENGTH_SHORT = Snackbar.LENGTH_SHORT
+
+    @JvmStatic
+    val LENGTH_LONG = Snackbar.LENGTH_LONG
+
+    @SuppressLint("Recycle")
+    @JvmStatic
+    fun make(view: View, text: CharSequence, duration: Int): BpkSnackbar {
+      val context = view.context
+
+      var textColor = BpkTheme.getColor(context, R.color.bpkWhite)
+      var actionColor = BpkTheme.getColor(context, R.color.bpkBlue500)
+      var backgroundColor = BpkTheme.getColor(context, R.color.bpkGray900)
+
+      val outValue = TypedValue()
+      context.theme.resolveAttribute(R.attr.bpkSnackbarStyle, outValue, true)
+
+      context.obtainStyledAttributes(outValue.resourceId, R.styleable.BpkSnackbar).use {
+        textColor = it.getColor(R.styleable.BpkSnackbar_snackbarTextColor, textColor)
+        actionColor = it.getColor(R.styleable.BpkSnackbar_snackbarActionColor, actionColor)
+        backgroundColor = it.getColor(R.styleable.BpkSnackbar_snackbarBackgroundColor, backgroundColor)
+      }
+
+      return BpkSnackbar(
+        snackbar = Snackbar.make(view, "", duration),
+        textColor = textColor,
+        actionColor = actionColor,
+        backgroundColor = backgroundColor
+      )
+        .setText(text)
+    }
+
+    @JvmStatic
+    fun make(view: View, @StringRes text: Int, duration: Int) =
+      make(view, view.resources.getString(text), duration)
+  }
+}
---
+++ app/src/main/java/net/skyscanner/backpack/demo/stories/SnackbarStory.kt
@@ -0,0 +1,44 @@
+package net.skyscanner.backpack.demo.stories
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.snackbar.BpkSnackbar
+
+class SnackbarStory : Story() {
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    view.findViewById<TextView>(R.id.snackbar_short).setOnClickListener {
+      it as TextView
+      BpkSnackbar.make(view, it.text, BpkSnackbar.LENGTH_SHORT)
+        .setAction("Action!") {}
+        .show()
+    }
+
+    view.findViewById<TextView>(R.id.snackbar_long).setOnClickListener {
+      it as TextView
+      BpkSnackbar.make(view, it.text, BpkSnackbar.LENGTH_LONG)
+        .setAction("Action!") {}
+        .show()
+    }
+
+    view.findViewById<TextView>(R.id.snackbar_indefinite).setOnClickListener {
+      it as TextView
+      BpkSnackbar.make(view, it.text, BpkSnackbar.LENGTH_INDEFINITE)
+        .setAction("Action!") {}
+        .show()
+    }
+  }
+
+  companion object {
+    private const val LAYOUT_ID = "fragment_id"
+
+    infix fun of(fragmentLayout: Int) = SnackbarStory().apply {
+      arguments = Bundle()
+      arguments?.putInt(LAYOUT_ID, fragmentLayout)
+    }
+  }
+}
