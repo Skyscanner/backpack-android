bb366e371a90a712333be4fe4760226f9d59e372
Tiago Engel <tiagohngl@gmail.com>
2019-08-19 16:45:33 +0100

    Update test

--- Backpack/src/androidTest/java/net/skyscanner/backpack/util/BpkThemeTest.kt
+++ Backpack/src/androidTest/java/net/skyscanner/backpack/util/BpkThemeTest.kt
@@ -1,6 +1,5 @@
 package net.skyscanner.backpack.util
 
-import android.content.Context
 import android.content.ContextWrapper
 import android.graphics.Color
 import android.util.TypedValue
@@ -25,20 +24,7 @@ class BpkThemeTest {
     ActivityTestRule(TestActivity::class.java)
 
   private val blue500 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkBlue500) }
-  private val gray50 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray50) }
-  private val gray100 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray100) }
-  private val gray300 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray300) }
-  private val gray500 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray500) }
-  private val gray700 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray700) }
-  private val gray900 by unsafeLazy { ContextCompat.getColor(activity, R.color.bpkGray900) }
-
   private val customBlue = Color.parseColor("#9B104A")
-  private val customGray50 = Color.parseColor("#9B104B")
-  private val customGray100 = Color.parseColor("#9B104C")
-  private val customGray300 = Color.parseColor("#9B104D")
-  private val customGray500 = Color.parseColor("#9B104E")
-  private val customGray700 = Color.parseColor("#9B104F")
-  private val customGray900 = Color.parseColor("#9B105A")
 
   @Before
   fun setUp() {
@@ -46,30 +32,13 @@ class BpkThemeTest {
   }
 
   @Test
-  fun test_colors() {
-    val allColors = listOf(
-      listOf("primary", blue500, customBlue, { c: Context -> BpkTheme.getPrimaryColor(c) }),
-      listOf("gray50", gray50, customGray50, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray50) }),
-      listOf("gray100", gray100, customGray100, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray100) }),
-      listOf("gray300", gray300, customGray300, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray300) }),
-      listOf("gray500", gray500, customGray500, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray500) }),
-      listOf("gray700", gray700, customGray700, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray700) }),
-      listOf("gray900", gray900, customGray900, { c: Context -> ContextCompat.getColor(c, R.color.bpkGray900) })
-    )
+  fun test_primary_color() {
+    Assert.assertEquals("default", blue500, BpkTheme.getPrimaryColor(activity))
 
-    allColors.forEach {
-      val desc = it[0] as String
-      val color = it[1] as Int
-      val customColor = it[2] as Int
-      val test = it[3] as (c: Context) -> Int
-
-      Assert.assertEquals("$desc default", color, test(activity))
-
-      Assert.assertEquals(
-        "$desc custom",
-        customColor,
-        test(ContextThemeWrapper(activity, R.style.TestThemeUtilsCustomColors)))
-    }
+    Assert.assertEquals(
+      "Themed",
+      customBlue,
+      BpkTheme.getPrimaryColor(ContextThemeWrapper(activity, R.style.TestThemeUtilsCustomColors)))
   }
 
   @Test
@@ -87,9 +56,6 @@ class BpkThemeTest {
     withThemeAndDefault.theme.resolveAttribute(R.attr.bpkPrimaryColor, t, true)
 
     Assert.assertEquals(customBlue, t.data)
-
-    withThemeAndDefault.theme.resolveAttribute(R.attr.bpkGray50Color, t, true)
-    Assert.assertEquals(gray50, t.data)
   }
 
   @Test
@@ -108,8 +74,5 @@ class BpkThemeTest {
     withTheme.theme.resolveAttribute(R.attr.bpkPrimaryColor, t, true)
 
     Assert.assertEquals(customBlue, t.data)
-
-    withTheme.theme.resolveAttribute(R.attr.bpkGray50Color, t, true)
-    Assert.assertEquals(gray50, t.data)
   }
 }
