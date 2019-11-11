851cff38c1f0e310c44183a4983b6f523054b1ff
Vitaliy Babichev <bvitaliyg@gmail.com>
2019-11-11 13:57:45 +0000

    Did some final adjustments on the Dark Mode. (#372)

--- Backpack/src/main/java/net/skyscanner/backpack/spinner/BpkSpinner.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/spinner/BpkSpinner.kt
@@ -41,7 +41,7 @@ open class BpkSpinner @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
   private val colors = arrayOf(
-    R.color.bpkSkyBlue,
+    R.color.bpkPrimary,
     R.color.bpkWhite,
     R.color.bpkSkyGrayTint01
   )
