640fd57219b49a109a01804903c078f52cdda57d
bvitaliyg <bvitaliyg@gmail.com>
2020-03-06 16:56:55 +0000

    [BPK-3722] Updated the tests.

--- app/src/androidTest/java/net/skyscanner/backpack/bottomnav/BpkBottomNavTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/bottomnav/BpkBottomNavTest.kt
@@ -5,7 +5,6 @@ import net.skyscanner.backpack.BpkSnapshotTest
 import net.skyscanner.backpack.createThemedContext
 import net.skyscanner.backpack.demo.R
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -23,18 +22,17 @@ class BpkBottomNavTest : BpkSnapshotTest() {
     snap(BpkBottomNav(testContext).apply {
       addItem(1, R.string.bottom_nav_home, R.drawable.bpk_hotels)
       addItem(2, R.string.bottom_nav_explore, R.drawable.bpk_navigation)
-      addItem(3, R.string.bottom_nav_trips, R.drawable.bpk_trips)
+      addItem(3, R.string.bottom_nav_trips, R.drawable.bpk_star)
       addItem(4, R.string.bottom_nav_profile, R.drawable.bpk_account_circle)
     })
   }
 
   @Test
-  @Ignore("This test works fine on a local machine but fails on the CI with the correct output images")
   fun screenshotTestBottomNav_Themed() {
     snap(BpkBottomNav(createThemedContext(testContext)).apply {
       addItem(1, R.string.bottom_nav_home, R.drawable.bpk_hotels)
       addItem(2, R.string.bottom_nav_explore, R.drawable.bpk_navigation)
-      addItem(3, R.string.bottom_nav_trips, R.drawable.bpk_trips)
+      addItem(3, R.string.bottom_nav_trips, R.drawable.bpk_star)
       addItem(4, R.string.bottom_nav_profile, R.drawable.bpk_account_circle)
     })
   }
