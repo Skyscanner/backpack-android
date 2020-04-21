2cf889df9e80d0dc68afe3467980c009d81970d5
Tiago Henrique Engel <tiagohngl@gmail.com>
2020-04-21 10:25:33 +0100

    [BPK-3840] Add icon prop to chip (#442)

--- app/src/androidTest/java/net/skyscanner/backpack/chip/BpkChipTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/chip/BpkChipTest.kt
@@ -1,11 +1,13 @@
 package net.skyscanner.backpack.chip
 
+import android.util.LayoutDirection
 import androidx.core.content.ContextCompat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import net.skyscanner.backpack.BpkSnapshotTest
 import net.skyscanner.backpack.createThemedContext
 import net.skyscanner.backpack.demo.R
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -61,6 +63,45 @@ class BpkChipTest : BpkSnapshotTest() {
     view.text = "tag"
     view.selectedBackgroundColor = ContextCompat.getColor(testContext, R.color.bpkPanjin)
     view.toggle()
+    snap(view)
+  }
+
+  @Test
+  @Ignore("TODO: relative drawables do not work with the screenshot test lib")
+  fun screenshotTestWithIcon() {
+    val view = BpkChip(testContext)
+    view.text = "tag"
+    view.icon = ContextCompat.getDrawable(testContext, R.drawable.bpk_account)
+    snap(view)
+  }
+
+  @Test
+  @Ignore("TODO: relative drawables do not work with the screenshot test lib")
+  fun screenshotTestSelectedWithIcon() {
+    val view = BpkChip(testContext)
+    view.text = "tag"
+    view.icon = ContextCompat.getDrawable(testContext, R.drawable.bpk_account)
+    view.isSelected = true
+    snap(view)
+  }
+
+  @Test
+  @Ignore("TODO: relative drawables do not work with the screenshot test lib")
+  fun screenshotTestDisabledWithIcon() {
+    val view = BpkChip(testContext)
+    view.text = "tag"
+    view.icon = ContextCompat.getDrawable(testContext, R.drawable.bpk_account)
+    view.disabled = true
+    snap(view)
+  }
+
+  @Test
+  @Ignore("TODO: relative drawables do not work with the screenshot test lib")
+  fun screenshotTestWithIcon_RTL() {
+    val view = BpkChip(testContext)
+    view.text = "tag"
+    view.icon = ContextCompat.getDrawable(testContext, R.drawable.bpk_account)
+    view.layoutDirection = LayoutDirection.RTL
     snap(view)
   }
 }
