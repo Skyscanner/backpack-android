b310ce59a26c2839da3cd80b16f814a8ff4a2fd5
Tiago Henrique Engel <tiagohngl@gmail.com>
2020-05-05 13:37:35 +0100

    [BPK-3436] Add license headers (#447)

--- scripts/generate_screenshots.sh
+++ scripts/generate_screenshots.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Backpack for Android - Skyscanner's Design System
+#
+# Copyright 2018-2020 Skyscanner Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 test_instrumentation_runner='net.skyscanner.backpack.BpkTestRunner'
 tests_apk_path='app/build/outputs/apk/androidTest/internal/debug/app-internal-debug-androidTest.apk'
