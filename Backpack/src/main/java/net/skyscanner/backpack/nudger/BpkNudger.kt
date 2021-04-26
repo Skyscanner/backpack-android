3e5a9cf84b89f32ccccf10f5b800319c26282aed
Maria Neumayer <maria.neumayer@skyscanner.net>
2021-04-26 14:04:07 +0100

    Update setter property names to value

--- Backpack/src/main/java/net/skyscanner/backpack/nudger/BpkNudger.kt
+++ Backpack/src/main/java/net/skyscanner/backpack/nudger/BpkNudger.kt
@@ -58,22 +58,22 @@ open class BpkNudger @JvmOverloads constructor(
     }
 
   var minValue: Int = 0
-    set(minValue) {
-      if (minValue > maxValue) {
-        throw IllegalArgumentException("Cannot set minValue $minValue when maxValue is $maxValue")
+    set(value) {
+      if (value > maxValue) {
+        throw IllegalArgumentException("Cannot set minValue $value when maxValue is $maxValue")
       }
-      field = minValue
-      value = max(value, minValue)
+      field = value
+      this.value = max(this.value, value)
       update()
     }
 
   var maxValue: Int = 100
-    set(maxValue) {
-      if (maxValue < minValue) {
-        throw IllegalArgumentException("Cannot set maxValue $maxValue when minValue is $minValue")
+    set(value) {
+      if (value < minValue) {
+        throw IllegalArgumentException("Cannot set maxValue $value when minValue is $minValue")
       }
-      field = maxValue
-      value = min(value, maxValue)
+      field = value
+      this.value = min(this.value, value)
       update()
     }
 
