c7a35699ddf74378907fbe8da57c8d0c222492d6
Maria Neumayer <neumayer.maria@gmail.com>
2021-05-10 17:01:26 +0100

    KOA-4582 Upgrade mockito android to 3.9.0 (#584)
    * KOA-4582 Update mockito android to 3.9.0
    
    * KOA-4582 Remove spy usage

--- Backpack/src/androidTest/java/net/skyscanner/backpack/calendar/presenter/BpkCalendarControllerTest.kt
+++ Backpack/src/androidTest/java/net/skyscanner/backpack/calendar/presenter/BpkCalendarControllerTest.kt
@@ -21,9 +21,6 @@ package net.skyscanner.backpack.calendar.presenter
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.jakewharton.threetenabp.AndroidThreeTen
-import com.nhaarman.mockitokotlin2.spy
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import java.util.Locale
 import net.skyscanner.backpack.calendar.model.CalendarRange
 import net.skyscanner.backpack.calendar.model.CalendarSelection
@@ -38,8 +35,10 @@ internal open class BpkCalendarControllerTestImpl(
   selectionType: SelectionType = SelectionType.RANGE
 ) : BpkCalendarController(selectionType) {
 
+  var currentRange: CalendarSelection? = null
+
   override val locale: Locale = Locale.forLanguageTag("pt-br")
-  override fun onRangeSelected(range: CalendarSelection) {}
+  override fun onRangeSelected(range: CalendarSelection) { currentRange = range }
 }
 
 @RunWith(AndroidJUnit4::class)
@@ -72,80 +71,74 @@ class BpkCalendarControllerTest {
 
   @Test
   fun test_onDayOfMonthSelected_when_first_selected() {
-    val spy = spy(subject)
     val day = LocalDate.of(2019, 1, 1)
     val expectedRange = CalendarRange(day, null)
 
-    spy.onDayOfMonthSelected(day)
-    verify(spy, times(1)).onRangeSelected(expectedRange)
+    subject.onDayOfMonthSelected(day)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
   fun test_onDayOfMonthSelected_when_selecting_range_end() {
-    val spy = spy(subject)
     val start = LocalDate.of(2019, 1, 1)
     val end = LocalDate.of(2019, 1, 4)
     val expectedRange = CalendarRange(start, end)
 
-    spy.onDayOfMonthSelected(start)
-    spy.onDayOfMonthSelected(end)
+    subject.onDayOfMonthSelected(start)
+    subject.onDayOfMonthSelected(end)
 
-    verify(spy, times(2)).onRangeSelected(expectedRange)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
   fun test_onDayOfMonthSelected_when_selecting_same_day() {
-    val spy = spy(subject)
     val start = LocalDate.of(2019, 1, 1)
     val expectedRange = CalendarRange(start, start)
 
-    spy.onDayOfMonthSelected(start)
-    spy.onDayOfMonthSelected(start)
+    subject.onDayOfMonthSelected(start)
+    subject.onDayOfMonthSelected(start)
 
-    verify(spy, times(2)).onRangeSelected(expectedRange)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
   fun test_onDayOfMonthSelected_when_selecting_same_day_thrice() {
-    val spy = spy(subject)
     val day = LocalDate.of(2019, 1, 1)
     val expectedRange = CalendarRange(null, null)
 
-    spy.onDayOfMonthSelected(day)
-    spy.onDayOfMonthSelected(day)
-    spy.onDayOfMonthSelected(day)
+    subject.onDayOfMonthSelected(day)
+    subject.onDayOfMonthSelected(day)
+    subject.onDayOfMonthSelected(day)
 
-    verify(spy, times(3)).onRangeSelected(expectedRange)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
   fun test_onDayOfMonthSelected_when_selecting_day_before_start_day() {
-    val spy = spy(subject)
     val start = LocalDate.of(2019, 1, 2)
     val end = LocalDate.of(2019, 1, 1)
 
     val expectedRange = CalendarRange(end, null)
 
-    spy.onDayOfMonthSelected(start)
-    spy.onDayOfMonthSelected(end)
+    subject.onDayOfMonthSelected(start)
+    subject.onDayOfMonthSelected(end)
 
-    verify(spy, times(2)).onRangeSelected(expectedRange)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
   fun test_onDayOfMonthSelected_when_selecting_different_range_end() {
-    val spy = spy(subject)
     val start = LocalDate.of(2019, 1, 1)
     val end1 = LocalDate.of(2019, 1, 4)
     val start2 = LocalDate.of(2019, 1, 3)
 
     val expectedRange = CalendarRange(start2, null)
 
-    spy.onDayOfMonthSelected(start)
-    spy.onDayOfMonthSelected(end1)
-    spy.onDayOfMonthSelected(start2)
+    subject.onDayOfMonthSelected(start)
+    subject.onDayOfMonthSelected(end1)
+    subject.onDayOfMonthSelected(start2)
 
-    verify(spy, times(3)).onRangeSelected(expectedRange)
+    Assert.assertEquals(expectedRange, subject.currentRange)
   }
 
   @Test
@@ -164,12 +157,12 @@ class BpkCalendarControllerTest {
   // region selection type Single day
   @Test
   fun test_onDayOfMonthSelected_whenSingleDaySelection() {
-    val spy = spy(BpkCalendarControllerTestImpl(SelectionType.SINGLE))
+    val subject = BpkCalendarControllerTestImpl(SelectionType.SINGLE)
     val selectedDay = LocalDate.of(2019, 4, 16)
 
-    spy.onDayOfMonthSelected(selectedDay)
+    subject.onDayOfMonthSelected(selectedDay)
 
-    verify(spy, times(1)).onRangeSelected(SingleDay(selectedDay))
+    Assert.assertEquals(SingleDay(selectedDay), subject.currentRange)
   }
 
   // endregion
