a43aef040e50360fe7e2333b1b141b08c27c31d2
Maria Neumayer <neumayer.maria@gmail.com>
2021-06-09 10:32:07 +0100

    KOA-4626 Add lint check for component usage (#598)
    * KOA-4626 Add lint check for component usage
    
    * KOA-4626 Update unreleased
    
    * KOA-4626 Add license header
    
    * KOA-4626 Include backpack url in report
    
    * KOA-4626 capitalise backpack

---
+++ backpack-lint/src/main/java/net/skyscanner/backpack/lint/check/BpkComponentUsageDetector.kt
@@ -0,0 +1,119 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018-2021 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.lint.check
+
+import com.android.resources.ResourceFolderType
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Context
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Location
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.android.tools.lint.detector.api.XmlContext
+import com.android.tools.lint.detector.api.XmlScanner
+import com.intellij.psi.PsiMethod
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.UClass
+import org.w3c.dom.Element
+
+@Suppress("UnstableApiUsage")
+class BpkComponentUsageDetector : Detector(), SourceCodeScanner, XmlScanner {
+
+  companion object {
+    val ISSUE = Issue.create(
+      id = "BpkComponentUsage",
+      briefDescription = "A Backpack version of this component is available",
+      explanation = "Use Backpack components where available to improve consistency, accessibility and dark mode support.",
+      category = Category.CORRECTNESS,
+      severity = Severity.WARNING,
+      implementation = Implementation(
+        BpkComponentUsageDetector::class.java,
+        Scope.JAVA_AND_RESOURCE_FILES
+      )
+    )
+
+    private val APPLICABLE_TYPES = Component.values().flatMap { it.componentsToReplace }
+  }
+
+  private val classCache = mutableListOf<String>()
+
+  override fun getApplicableConstructorTypes(): List<String> = APPLICABLE_TYPES
+
+  override fun applicableSuperClasses(): List<String> = APPLICABLE_TYPES
+
+  override fun getApplicableElements(): Collection<String> = APPLICABLE_TYPES
+
+  override fun appliesTo(folderType: ResourceFolderType): Boolean = folderType == ResourceFolderType.LAYOUT
+
+  override fun visitClass(context: JavaContext, declaration: UClass) {
+    val superClass = declaration.javaPsi.superClass?.qualifiedName ?: return
+    val component = Component.find(superClass)
+    if (component != null) {
+      if (declaration.name == null || classCache.contains(declaration.name)) {
+        // in some cases we might have two relevant super classes (e.g. when there's an AppCompat and default component) - only report issue once
+        return
+      }
+      classCache.add(declaration.name!!)
+      context.report(context.getNameLocation(declaration), superClass, component)
+    }
+  }
+
+  override fun visitConstructor(context: JavaContext, node: UCallExpression, constructor: PsiMethod) {
+    val className = node.returnType?.canonicalText ?: return
+    val component = Component.find(className)
+    if (component != null) {
+      context.report(context.getLocation(node), className, component)
+    }
+  }
+
+  override fun visitElement(context: XmlContext, element: Element) {
+    val component = Component.find(element.tagName)
+    if (component != null) {
+      context.report(context.getLocation(element), element.tagName, component)
+    }
+  }
+
+  private fun Context.report(location: Location, existingComponent: String, component: Component) {
+    report(
+      ISSUE,
+      location,
+      "Backpack component available for $existingComponent. Use ${component.fullName} instead. More info at ${component.url}"
+    )
+  }
+
+  private enum class Component(val fullName: String, val webName: String, val componentsToReplace: Set<String>) {
+    BUTTON(
+      fullName = "net.skyscanner.backpack.BpkButton",
+      webName = "button",
+      componentsToReplace = setOf("android.widget.Button", "Button", "androidx.appcompat.widget.AppCompatButton")
+    ),
+    ;
+
+    val url: String = "https://backpack.github.io/components/$webName"
+
+    companion object {
+      internal fun find(componentToReplace: String) =
+        values().firstOrNull { it.componentsToReplace.contains(componentToReplace) }
+    }
+  }
+}
---
+++ backpack-lint/src/test/java/net/skyscanner/backpack/lint/check/BpkComponentUsageDetectorTest.kt
@@ -0,0 +1,152 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018-2021 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.lint.check
+
+import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
+import com.android.tools.lint.checks.infrastructure.TestFiles.xml
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+@Suppress("UnstableApiUsage")
+class BpkComponentUsageDetectorTest {
+
+  @Test
+  fun `warning when extending native component`() {
+    lint()
+      .files(
+        kotlin(
+          """import android.widget.Button
+import android.content.Context
+
+class CustomButton(context: Context) : Button(context)"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectWarningCount(1)
+      .expect(
+        """
+src/CustomButton.kt:4: Warning: Backpack component available for android.widget.Button. Use net.skyscanner.backpack.BpkButton instead. More info at https://backpack.github.io/components/button [BpkComponentUsage]
+class CustomButton(context: Context) : Button(context)
+      ~~~~~~~~~~~~
+0 errors, 1 warnings
+      """
+      )
+  }
+
+  @Test
+  fun `warning when instantiating native component`() {
+    lint()
+      .files(
+        kotlin(
+          """import android.widget.Button
+
+class View(context: Context) {
+  private val button = Button(context)
+}"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectWarningCount(1)
+      .expect(
+        """
+src/View.kt:4: Warning: Backpack component available for android.widget.Button. Use net.skyscanner.backpack.BpkButton instead. More info at https://backpack.github.io/components/button [BpkComponentUsage]
+  private val button = Button(context)
+                       ~~~~~~~~~~~~~~~
+0 errors, 1 warnings
+      """
+      )
+  }
+
+  @Test
+  fun `warning when using native component in xml`() {
+    lint()
+      .files(
+        xml(
+          "res/layout/native_button.xml",
+          """<?xml version="1.0" encoding="utf-8"?>
+<Button xmlns:android="http://schemas.android.com/apk/res/android"
+  android:layout_width="wrap_content"
+  android:layout_height="wrap_content" />"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectWarningCount(1)
+      .expect(
+        """
+res/layout/native_button.xml:2: Warning: Backpack component available for Button. Use net.skyscanner.backpack.BpkButton instead. More info at https://backpack.github.io/components/button [BpkComponentUsage]
+<Button xmlns:android="http://schemas.android.com/apk/res/android"
+^
+0 errors, 1 warnings
+      """
+      )
+  }
+
+  @Test
+  fun `clean when extending bpk component`() {
+    lint()
+      .files(
+        kotlin(
+          """import net.skyscanner.backpack.BpkButton
+import android.content.Context
+
+class CustomButton(context: Context) : BpkButton(context)"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectClean()
+  }
+
+  @Test
+  fun `clean when instantiating bpk component`() {
+    lint()
+      .files(
+        kotlin(
+          """import net.skyscanner.backpack.BpkButton
+
+class View(context: Context) {
+private val button = BpkButton(context)
+}"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectClean()
+  }
+
+  @Test
+  fun `clean when using bpk component in xml`() {
+    lint()
+      .files(
+        xml(
+          "res/layout/backpack_button.xml",
+          """<?xml version="1.0" encoding="utf-8"?>
+<net.skyscanner.backpack.BpkButton xmlns:android="http://schemas.android.com/apk/res/android"
+  android:layout_width="wrap_content"
+  android:layout_height="wrap_content" />"""
+        )
+      )
+      .issues(BpkComponentUsageDetector.ISSUE)
+      .run()
+      .expectClean()
+  }
+}
