99870e9fc6b2be91825211c5329acb38d455d19b
bvitaliyg <bvitaliyg@gmail.com>
2021-07-13 21:10:02 +0300

    Added tests for leading/trailing dates in range.

--- Backpack/src/test/java/net/skyscanner/backpack/calendar2/data/CalendarCellsLayoutTests.kt
+++ Backpack/src/test/java/net/skyscanner/backpack/calendar2/data/CalendarCellsLayoutTests.kt
@@ -3,6 +3,7 @@ package net.skyscanner.backpack.calendar2.data
 import net.skyscanner.backpack.calendar2.CalendarSettings
 import net.skyscanner.backpack.calendar2.testCalendarWith
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.threeten.bp.Month
@@ -34,6 +35,60 @@ class CalendarCellsLayoutTests {
   }
 
   @Test
+  fun `there are trailing spaces in month`() {
+    testCalendarWith(CalendarSettings.Default) {
+      verify {
+        assertTrue(state.cells[37] is CalendarCell.Day)
+        assertTrue(state.cells[38] is CalendarCell.Space)
+        assertTrue(state.cells[39] is CalendarCell.Space)
+        assertTrue(state.cells[40] is CalendarCell.Space)
+        assertTrue(state.cells[41] is CalendarCell.Space)
+        assertTrue(state.cells[42] is CalendarCell.Space)
+      }
+    }
+  }
+
+  @Test
+  fun `there are leading dates in first month of range`() {
+    testCalendarWith(
+      CalendarSettings.Default.copy(
+        range = CalendarSettings.Default.range.start.plusDays(3)..CalendarSettings.Default.range.endInclusive,
+      )
+    ) {
+      verify {
+        assertEquals(1, (state.cells[7] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(2, (state.cells[8] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(3, (state.cells[9] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(4, (state.cells[10] as CalendarCell.Day).date.dayOfMonth)
+        assertTrue((state.cells[7] as CalendarCell.Day).outOfRange)
+        assertTrue((state.cells[8] as CalendarCell.Day).outOfRange)
+        assertTrue((state.cells[9] as CalendarCell.Day).outOfRange)
+        assertFalse((state.cells[10] as CalendarCell.Day).outOfRange)
+      }
+    }
+  }
+
+  @Test
+  fun `there are trailing dates in last month of range`() {
+    testCalendarWith(
+      CalendarSettings.Default.copy(
+        range = CalendarSettings.Default.range.start..CalendarSettings.Default.range.start.plusDays(27),
+      )
+    ) {
+      verify {
+        assertEquals(31, (state.cells[37] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(30, (state.cells[36] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(29, (state.cells[35] as CalendarCell.Day).date.dayOfMonth)
+        assertEquals(28, (state.cells[34] as CalendarCell.Day).date.dayOfMonth)
+        assertTrue((state.cells[37] as CalendarCell.Day).outOfRange)
+        assertTrue((state.cells[36] as CalendarCell.Day).outOfRange)
+        assertTrue((state.cells[35] as CalendarCell.Day).outOfRange)
+        assertFalse((state.cells[34] as CalendarCell.Day).outOfRange)
+      }
+    }
+  }
+
+  @Test
   fun `there are correct days in month`() {
     testCalendarWith(CalendarSettings.Default) {
       verify {
@@ -46,20 +101,6 @@ class CalendarCellsLayoutTests {
           assertEquals(Month.JANUARY, cell.date.month)
           assertEquals(2000, cell.date.year)
         }
-      }
-    }
-  }
-
-  @Test
-  fun `there are trailing spaces in month`() {
-    testCalendarWith(CalendarSettings.Default) {
-      verify {
-        assertTrue(state.cells[37] is CalendarCell.Day)
-        assertTrue(state.cells[38] is CalendarCell.Space)
-        assertTrue(state.cells[39] is CalendarCell.Space)
-        assertTrue(state.cells[40] is CalendarCell.Space)
-        assertTrue(state.cells[41] is CalendarCell.Space)
-        assertTrue(state.cells[42] is CalendarCell.Space)
       }
     }
   }
