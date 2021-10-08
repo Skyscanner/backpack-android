e6e453453629c99353b4b3f897253801697193a5
bvitaliyg <bvitaliyg@gmail.com>
2021-10-08 11:11:40 +0100

    Added copyright support.

--- buildSrc/src/main/kotlin/net/skyscanner/backpack/tokens/BpkOutput.kt
+++ buildSrc/src/main/kotlin/net/skyscanner/backpack/tokens/BpkOutput.kt
@@ -1,10 +1,13 @@
 package net.skyscanner.backpack.tokens
 
+import com.google.common.io.Resources
 import com.squareup.kotlinpoet.AnnotationSpec
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.TypeSpec
 import java.io.File
+import java.nio.charset.StandardCharsets
+import java.util.*
 
 fun <T> Pipeline<T>.saveTo(output: BpkOutput<T>): Pipeline<Boolean> =
   pipeTo(output)
@@ -17,8 +20,12 @@ sealed class BpkOutput<Input> : (Input) -> Boolean {
   ) : BpkOutput<TypeSpec>() {
 
     override fun invoke(typeSpec: TypeSpec): Boolean {
+      val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+      val copyright = Resources.toString(Resources.getResource("copyright.txt"), StandardCharsets.UTF_8)
       FileSpec.builder(pkg, typeSpec.name!!)
+        .addComment(copyright, currentYear)
         .suppressWarningTypes("RedundantVisibilityModifier", "unused")
+        .addComment("Auto-generated: do not edit")
         .addType(typeSpec)
         .build()
         .writeTo(File(srcDir))
