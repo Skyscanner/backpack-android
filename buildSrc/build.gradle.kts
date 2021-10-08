f67c62452c79a193b57ded100ba24f09868fb4d7
bvitaliyg <bvitaliyg@gmail.com>
2021-10-08 15:39:58 +0100

    Kotlin version fix.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,9 +8,10 @@ plugins {
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:7.0.2")
-  implementation(kotlin("gradle-plugin", "1.5.30"))
   implementation(gradleApi())
+  implementation("com.android.tools.build:gradle:7.0.2")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
   implementation("com.squareup:kotlinpoet:1.10.1")
   implementation("com.google.guava:guava:31.0.1-jre")
+  implementation("com.google.code.gson:gson:2.8.8")
 }
