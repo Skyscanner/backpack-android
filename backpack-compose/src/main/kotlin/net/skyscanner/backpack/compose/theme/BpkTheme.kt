af8016cd1865542239780fe75ffa4d680cae904d
Vitaliy Babichev <bvitaliyg@gmail.com>
2021-11-15 17:05:17 +0000

    Koa 4789 dev android create base jetpack compose theme (#777)
    * Added BpkTheme colours mapping.
    
    * Added BpkTheme colours mapping.
    
    * Added theme sample.
    
    * Added shapes into BpkTheme.kt
    
    * Added license header to ThemeStory.kt
    
    * Added shapes accessor, made theme composition local private
    
    * Added empty elevation overlay.
    
    * Typo fix

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/theme/BpkTheme.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/theme/BpkTheme.kt
@@ -18,17 +18,28 @@
 
 package net.skyscanner.backpack.compose.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Colors
+import androidx.compose.material.LocalElevationOverlay
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
+import net.skyscanner.backpack.compose.tokens.BpkColor
+import net.skyscanner.backpack.compose.tokens.BpkColors
 import net.skyscanner.backpack.compose.tokens.BpkTypography
 
-val LocalBpkTypography = staticCompositionLocalOf {
-  BpkTypography()
+private val LocalBpkTypography = staticCompositionLocalOf<BpkTypography> {
+  error("Wrap you content with BpkTheme {} to get access to Backpack typography")
+}
+private val LocalBpkColors = staticCompositionLocalOf<BpkColors> {
+  error("Wrap you content with BpkTheme {} to get access to Backpack colors")
 }
 
 @Composable
@@ -37,34 +48,76 @@ fun BpkTheme(
   content: @Composable () -> Unit,
 ) {
   val typography = BpkTypography(defaultFontFamily = fontFamily)
+  val colors = if (isSystemInDarkTheme()) BpkColors.dark() else BpkColors.light()
+
   CompositionLocalProvider(
     LocalBpkTypography provides typography,
+    LocalBpkColors provides colors,
+    LocalElevationOverlay provides null,
   ) {
-    val materialTypography = Typography(
-      defaultFontFamily = fontFamily,
-      h1 = typography.hero2,
-      h2 = typography.hero4,
-      h3 = typography.hero5,
-      h4 = typography.heading2,
-      h5 = typography.heading3,
-      h6 = typography.heading4,
-      subtitle1 = typography.bodyDefault,
-      subtitle2 = typography.footnote,
-      body1 = typography.bodyDefault,
-      body2 = typography.footnote,
-      button = typography.label2,
-      caption = typography.caption,
-      overline = typography.caption.copy(fontWeight = FontWeight.Bold),
-    )
     MaterialTheme(
-      typography = materialTypography,
+      typography = typography.toMaterialTypography(fontFamily),
+      colors = colors.toMaterialColors(),
+      shapes = bpkShapes(),
       content = content,
     )
   }
 }
 
 object BpkTheme {
+
   val typography: BpkTypography
     @Composable
     get() = LocalBpkTypography.current
+
+  val colors: BpkColors
+    @Composable
+    get() = LocalBpkColors.current
+
+  val shapes: Shapes
+    @Composable
+    get() = MaterialTheme.shapes
+
 }
+
+private fun bpkShapes() : Shapes =
+  Shapes(
+    small = RoundedCornerShape(BpkBorderRadius.Sm),
+    medium = RoundedCornerShape(BpkBorderRadius.Md),
+    large = RoundedCornerShape(BpkBorderRadius.Lg),
+  )
+
+private fun BpkTypography.toMaterialTypography(fontFamily: FontFamily) : Typography =
+  Typography(
+    defaultFontFamily = fontFamily,
+    h1 = hero2,
+    h2 = hero4,
+    h3 = hero5,
+    h4 = heading2,
+    h5 = heading3,
+    h6 = heading4,
+    subtitle1 = bodyDefault,
+    subtitle2 = footnote,
+    body1 = bodyDefault,
+    body2 = footnote,
+    button = label2,
+    caption = caption,
+    overline = caption.copy(fontWeight = FontWeight.Bold),
+  )
+
+private fun BpkColors.toMaterialColors(): Colors =
+  Colors(
+    primary = primary,
+    primaryVariant = if (isLight) BpkColor.SkyBlueShade01 else BpkColor.SkyBlueTint01,
+    secondary = BpkColor.Monteverde,
+    secondaryVariant = BpkColor.Glencoe,
+    background = background,
+    surface = backgroundElevation01,
+    error = if (isLight) BpkColor.Panjin else BpkColor.Hillier,
+    onPrimary = BpkColor.White,
+    onSecondary = BpkColor.White,
+    onBackground = textPrimary,
+    onSurface = textPrimary,
+    onError = BpkColor.White,
+    isLight = isLight,
+  )
