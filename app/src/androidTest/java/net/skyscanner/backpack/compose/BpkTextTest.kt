ab3822ac487ef9487de01c4f4c52cf06cb55120f
bvitaliyg <bvitaliyg@gmail.com>
2021-11-29 17:08:23 +0000

    Review changes.

--- app/src/androidTest/java/net/skyscanner/backpack/compose/BpkTextTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/compose/BpkTextTest.kt
@@ -2,12 +2,12 @@ package net.skyscanner.backpack.compose
 
 import androidx.compose.material.Text
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import net.skyscanner.backpack.BpkComposeTest
+import net.skyscanner.backpack.BpkSnapshotTest
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BpkTextTest : BpkComposeTest {
+class BpkTextTest : BpkSnapshotTest() {
 
   @Test
   fun default() = composed {
