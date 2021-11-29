bbdadf51eedb06fc8514d0ed00050b2d04e5b2a3
bvitaliyg <bvitaliyg@gmail.com>
2021-11-29 16:04:11 +0000

    Moved the code to ActivityScenario

--- app/src/androidTest/java/net/skyscanner/backpack/BpkComposeTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/BpkComposeTest.kt
@@ -1,5 +1,6 @@
 package net.skyscanner.backpack
 
+import android.view.ViewGroup
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,25 +11,17 @@ import androidx.compose.runtime.ProvidedValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.takeOrElse
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.rule.ActivityTestRule
+import androidx.test.core.app.ActivityScenario
 import com.facebook.testing.screenshot.Screenshot
 import com.facebook.testing.screenshot.ViewHelpers
 import com.facebook.testing.screenshot.internal.TestNameDetector.getTestClass
 import com.facebook.testing.screenshot.internal.TestNameDetector.getTestName
 import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.demo.MainActivity
-import org.junit.Rule
 
-abstract class BpkComposeTest {
-
-  @get:Rule
-  var activityRule: ActivityTestRule<MainActivity> =
-    ActivityTestRule(MainActivity::class.java)
+interface BpkComposeTest {
 
   fun composed(
     size: IntSize = IntSize(100, 100),
@@ -39,37 +32,34 @@ abstract class BpkComposeTest {
 
     val screenshotName = "${getTestClass().removePrefix("net.skyscanner.backpack.")}_${getTestName()}"
 
-    with(activityRule.activity) {
-      runOnUiThread {
-        setContent {
-          BpkTheme {
-            CompositionLocalProvider(*providers) {
-              Box(
-                modifier = Modifier
-                  .size(size.width.dp, size.height.dp)
-                  .background(background.takeOrElse { BpkTheme.colors.background }),
-                content = { content() },
-              )
+    ActivityScenario.launch(MainActivity::class.java).use { scenario ->
+      scenario.onActivity { activity ->
+        with(activity) {
+          setContent {
+            BpkTheme {
+              CompositionLocalProvider(*providers) {
+                Box(
+                  modifier = Modifier
+                    .size(size.width.dp, size.height.dp)
+                    .background(background.takeOrElse { BpkTheme.colors.background }),
+                  content = { content() },
+                )
+              }
             }
           }
+
+          val view = (window.decorView as ViewGroup).getChildAt(0)
+
+          ViewHelpers.setupView(view)
+            .setExactWidthDp(size.width)
+            .setExactHeightDp(size.height)
+            .layout()
+
+          Screenshot.snap(view)
+            .setName(screenshotName)
+            .record()
         }
       }
     }
-
-    Espresso.onView(ViewMatchers.isAssignableFrom(ComposeView::class.java))
-      .check { view, noViewFoundException ->
-        if (noViewFoundException != null) {
-          throw noViewFoundException
-        }
-
-        ViewHelpers.setupView(view)
-          .setExactWidthDp(size.width)
-          .setExactHeightDp(size.height)
-          .layout()
-
-        Screenshot.snap(view)
-          .setName(screenshotName)
-          .record()
-      }
   }
 }
--- app/src/androidTest/java/net/skyscanner/backpack/compose/BpkTextTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/compose/BpkTextTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BpkTextTest : BpkComposeTest() {
+class BpkTextTest : BpkComposeTest {
 
   @Test
   fun default() = composed {
