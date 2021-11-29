491a7ec1e77a2d523f2c778213ea09c370e87b02
bvitaliyg <bvitaliyg@gmail.com>
2021-11-29 15:45:56 +0000

    Working prototype.

--- app/src/androidTest/java/net/skyscanner/backpack/BpkComposeTest.kt
+++ app/src/androidTest/java/net/skyscanner/backpack/BpkComposeTest.kt
@@ -1,66 +1,75 @@
 package net.skyscanner.backpack
 
-import android.os.Looper
-import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ProvidedValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
 import com.facebook.testing.screenshot.Screenshot
-import com.facebook.testing.screenshot.internal.TestNameDetector
+import com.facebook.testing.screenshot.ViewHelpers
+import com.facebook.testing.screenshot.internal.TestNameDetector.getTestClass
+import com.facebook.testing.screenshot.internal.TestNameDetector.getTestName
 import net.skyscanner.backpack.compose.theme.BpkTheme
+import net.skyscanner.backpack.demo.MainActivity
+import org.junit.Rule
 
 abstract class BpkComposeTest {
 
-  fun composable(
-    activity: ComponentActivity,
-    width: Dp = 100.dp,
-    height: Dp = 100.dp,
-    variant: BpkTestVariant = BpkTestVariant.current,
+  @get:Rule
+  var activityRule: ActivityTestRule<MainActivity> =
+    ActivityTestRule(MainActivity::class.java)
+
+  fun composed(
+    size: IntSize = IntSize(100, 100),
     background: Color = Color.Unspecified,
+    vararg providers: ProvidedValue<*>,
     content: @Composable () -> Unit,
   ) {
 
-    if (Looper.myLooper() == null) {
-      Looper.prepare()
-    }
+    val screenshotName = "${getTestClass().removePrefix("net.skyscanner.backpack.")}_${getTestName()}"
 
-    val testClass = TestNameDetector.getTestClass()
-    val testName = TestNameDetector.getTestName()
-
-    activity.runOnUiThread {
-      val context = variant.newContext(InstrumentationRegistry.getInstrumentation().targetContext)
-      val view = ComposeView(context)
-      activity.setContentView(view)
-
-      view.setContent {
-        BpkTheme {
-          Box(
-            Modifier
-              .size(width, height)
-              .background(background.takeOrElse { BpkTheme.colors.background })
-          ) {
-            content()
+    with(activityRule.activity) {
+      runOnUiThread {
+        setContent {
+          BpkTheme {
+            CompositionLocalProvider(*providers) {
+              Box(
+                modifier = Modifier
+                  .size(size.width.dp, size.height.dp)
+                  .background(background.takeOrElse { BpkTheme.colors.background }),
+                content = { content() },
+              )
+            }
           }
         }
       }
-
-      Screenshot.snap(view)
-        .setName(getScreenshotName(testClass, testName))
-        .record()
     }
-  }
 
-  private fun getScreenshotName(
-    testClass: String = TestNameDetector.getTestClass(),
-    testName: String = TestNameDetector.getTestName(),
-  ): String =
-    "${testClass.removePrefix("net.skyscanner.backpack.")}_$testName"
+    Espresso.onView(ViewMatchers.isAssignableFrom(ComposeView::class.java))
+      .check { view, noViewFoundException ->
+        if (noViewFoundException != null) {
+          throw noViewFoundException
+        }
+
+        ViewHelpers.setupView(view)
+          .setExactWidthDp(size.width)
+          .setExactHeightDp(size.height)
+          .layout()
+
+        Screenshot.snap(view)
+          .setName(screenshotName)
+          .record()
+      }
+  }
 }
