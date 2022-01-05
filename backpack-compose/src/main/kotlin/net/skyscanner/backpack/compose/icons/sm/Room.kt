49ab7f40e78711550d8293af1d090843a3c77f8b
Maria Neumayer <maria.neumayer@skyscanner.net>
2022-01-05 10:45:29 +0000

    Update generated license headers (#840)

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/icons/sm/Room.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/icons/sm/Room.kt
@@ -1,5 +1,5 @@
 // Backpack for Android - Skyscanner's Design System
-// Copyright 2018-2021 Skyscanner Ltd
+// Copyright 2018-2022 Skyscanner Ltd
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
