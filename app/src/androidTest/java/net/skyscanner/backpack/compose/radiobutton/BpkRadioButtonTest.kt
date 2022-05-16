31dcb76cb4f91399decace9a81285a06cec96d6b
Maria Neumayer <maria.neumayer@skyscanner.net>
2022-05-16 15:35:27 +0100

    KOA-4927 Add tests and docs

---
+++ app/src/androidTest/java/net/skyscanner/backpack/compose/radiobutton/BpkRadioButtonTest.kt
@@ -0,0 +1,60 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.radiobutton
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import net.skyscanner.backpack.BpkSnapshotTest
+import net.skyscanner.backpack.demo.compose.DefaultCheckedRadioButtonExample
+import net.skyscanner.backpack.demo.compose.DefaultUncheckedRadioButtonExample
+import net.skyscanner.backpack.demo.compose.DisabledCheckedRadioButtonExample
+import net.skyscanner.backpack.demo.compose.DisabledUnCheckedRadioButtonExample
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BpkRadioButtonTest : BpkSnapshotTest() {
+
+  @Before
+  fun setup() {
+    setDimensions(height = 50, width = 200)
+  }
+
+  @Test
+  fun defaultUnchecked() {
+    composed {
+      DefaultUncheckedRadioButtonExample()
+    }
+  }
+
+  @Test
+  fun defaultChecked() = composed {
+    DefaultCheckedRadioButtonExample()
+  }
+
+  @Test
+  fun disabledUnchecked() = composed {
+    DisabledUnCheckedRadioButtonExample()
+  }
+
+  @Test
+  fun disabledChecked() = composed {
+    DisabledCheckedRadioButtonExample()
+  }
+}
