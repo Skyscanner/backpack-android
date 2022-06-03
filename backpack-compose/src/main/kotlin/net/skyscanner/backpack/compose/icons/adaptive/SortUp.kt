7fa1e1e4610e8570bc2d1a1fc7f326e44ee50610
bvitaliyg <bvitaliyg@gmail.com>
2022-06-03 17:45:25 +0100

    Generated adaptive icons.

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/icons/adaptive/SortUp.kt
@@ -0,0 +1,29 @@
+// Backpack for Android - Skyscanner's Design System
+// Copyright 2018 Skyscanner Ltd
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Auto-generated: do not edit
+@file:Suppress("RedundantVisibilityModifier","unused")
+
+package net.skyscanner.backpack.compose.icons.adaptive
+
+import kotlin.properties.Delegates
+import net.skyscanner.backpack.compose.R
+import net.skyscanner.backpack.compose.icon.BpkIcon
+import net.skyscanner.backpack.compose.icons.BpkIcons
+import net.skyscanner.backpack.compose.utils.singleton
+
+public val BpkIcons.SortUp: BpkIcon by Delegates.singleton(
+  BpkIcon(
+    small = R.drawable.bpk_sort_up_sm,
+    large = R.drawable.bpk_sort_up,
+  )
+)
+
