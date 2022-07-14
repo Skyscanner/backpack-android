f68b4f8b1d2ac3e2feaf09d8baa137b950b20529
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-14 14:18:27 +0100

    Add list of tokens

--- app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
@@ -3,13 +3,19 @@ package net.skyscanner.backpack.demo.compose
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import net.skyscanner.backpack.compose.tokens.BpkColor
+import net.skyscanner.backpack.demo.data.Token
+import net.skyscanner.backpack.demo.data.values
 
 @Composable
 @Preview
 fun ColorsComposeStory() {
   Column {
-    Text(text = "Any text")
-    Text(text = "More text")
+    val data: List<Token<Color>> = BpkColor.values
+    for (item: Token<Color> in data) {
+      Text(text = item.name)
+    }
   }
 }
