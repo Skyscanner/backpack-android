fa89945f7e17c2aea06fa42ad4fa7d0d167665b7
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-14 16:30:45 +0100

    Review fixes

--- app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
@@ -1,3 +1,22 @@
+/*
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package net.skyscanner.backpack.demo.compose
 
 import androidx.compose.foundation.background
@@ -36,15 +55,14 @@ fun ColorsComposeStory() {
 @Composable
 private fun ColorSampleRow(token: Token<Color>) {
   Row(
-    modifier = Modifier
-      .height(56.dp),
+    modifier = Modifier.height(56.dp),
     verticalAlignment = Alignment.CenterVertically,
   ) {
     BpkText(
       text = token.name,
       modifier = Modifier
         .weight(1f)
-        .padding(BpkSpacing.Base)
+        .padding(BpkSpacing.Base),
     )
     Box(
       modifier = Modifier
@@ -52,11 +70,11 @@ private fun ColorSampleRow(token: Token<Color>) {
         .width(112.dp)
         .padding(1.dp)
         .background(token.value),
-      contentAlignment = Alignment.Center
+      contentAlignment = Alignment.Center,
     ) {
       BpkText(
         text = colorToHex(color = token.value),
-        color = if (token.value.luminance() > 0.5) Color.Black else Color.White
+        color = if (token.value.luminance() > 0.5) Color.Black else Color.White,
       )
     }
   }
