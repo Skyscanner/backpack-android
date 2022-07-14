265323d879cfbc4c5dab9291d7bfeda1f4e95324
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-14 14:48:03 +0100

    Add padding and align text

--- app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/ColorsStory.kt
@@ -2,11 +2,17 @@ package net.skyscanner.backpack.demo.compose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.tokens.BpkColor
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.demo.data.Token
 import net.skyscanner.backpack.demo.data.values
 
@@ -23,7 +29,12 @@ fun ColorsComposeStory() {
 
 @Composable
 private fun ColorSampleRow(token: Token<Color>) {
-  Row() {
-    Text(text = token.name)
+  Row(
+    modifier = Modifier.height(48.dp)
+      .padding(BpkSpacing.Base),
+    verticalAlignment = Alignment.CenterVertically,
+
+  ) {
+    BpkText(text = token.name)
   }
 }
