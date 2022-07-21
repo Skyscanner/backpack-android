902990ae94b4c9a0e9c8a333a7ab4cd9efaa9390
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-21 15:00:43 +0100

    Add cards for different elevations

--- app/src/main/java/net/skyscanner/backpack/demo/compose/ElevationStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/ElevationStory.kt
@@ -19,13 +19,50 @@
 
 package net.skyscanner.backpack.demo.compose
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import net.skyscanner.backpack.compose.text.BpkText
+import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
+import net.skyscanner.backpack.compose.tokens.BpkColor
+import net.skyscanner.backpack.compose.tokens.BpkElevation
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
+import net.skyscanner.backpack.demo.data.Token
+import net.skyscanner.backpack.demo.data.values
 
 @Composable
 @Preview
 fun ElevationComposeStory() {
-  LazyColumn() {
+  Column() {
+    val data: List<Token<Dp>> = BpkElevation.values
+    for (item in data) {
+      ElevationCard(token = item)
+    }
+  }
+}
+
+@Composable
+private fun ElevationCard(token: Token<Dp>) {
+  Box(
+    modifier = Modifier
+      .fillMaxWidth()
+      .shadow(token.value, shape = RoundedCornerShape(BpkBorderRadius.Md))
+      .background(color = BpkColor.White, shape = RoundedCornerShape(BpkBorderRadius.Md)),
+  ) {
+    BpkText(
+      text = "${token.name} = ${token.value}",
+      modifier = Modifier
+        .align(Alignment.Center)
+        .padding(BpkSpacing.Base)
+    )
   }
 }
