607f5b64c1e841dab58053c2dd5dc37051f0901e
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-22 17:16:01 +0100

    Fix dark mode

--- app/src/main/java/net/skyscanner/backpack/demo/compose/ElevationStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/ElevationStory.kt
@@ -19,22 +19,20 @@
 
 package net.skyscanner.backpack.demo.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.max
 import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
-import net.skyscanner.backpack.compose.tokens.BpkColor
 import net.skyscanner.backpack.compose.tokens.BpkElevation
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.demo.data.Token
@@ -43,8 +41,8 @@ import net.skyscanner.backpack.demo.data.values
 @Composable
 @Preview
 fun ElevationComposeStory() {
-  Column() {
-    val data: List<Token<Dp>> = BpkElevation.values.sortedBy { it.value }
+  Column(modifier = Modifier.padding(vertical = BpkSpacing.Base)) {
+    val data = BpkElevation.values.sortedBy { it.value }
     for (item in data) {
       ElevationSample(token = item)
     }
@@ -53,27 +51,23 @@ fun ElevationComposeStory() {
 
 @Composable
 private fun ElevationCard(token: Token<Dp>) {
-  Box(
-    modifier = Modifier
-      .fillMaxWidth()
-      .shadow(token.value, shape = RoundedCornerShape(BpkBorderRadius.Md))
-      .background(color = BpkColor.White, shape = RoundedCornerShape(BpkBorderRadius.Md)),
+  Surface(
+    modifier = Modifier.fillMaxWidth(),
+    shape = RoundedCornerShape(BpkBorderRadius.Md),
+    elevation = token.value,
   ) {
-    BpkText(
-      text = "${token.name} = ${token.value}",
-      modifier = Modifier
-        .align(Alignment.Center)
-        .padding(BpkSpacing.Base),
-    )
+    Box(contentAlignment = Alignment.Center) {
+      BpkText(
+        text = "${token.name} = ${token.value}",
+        modifier = Modifier.padding(BpkSpacing.Base),
+      )
+    }
   }
 }
 
 @Composable
 private fun ElevationSample(token: Token<Dp>) {
-  Box(
-    modifier = Modifier
-      .padding(horizontal = max(BpkSpacing.Base, token.value), vertical = max(BpkSpacing.Base, token.value))
-  ) {
+  Box(Modifier.padding(horizontal = max(BpkSpacing.Base, token.value), vertical = max(BpkSpacing.Base, token.value))) {
     ElevationCard(token)
   }
 }
