ee2b51517d3d180340da7318b53f907833123d3f
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-07-25 17:08:45 +0100

    Add radii and style

--- app/src/main/java/net/skyscanner/backpack/demo/compose/RadiiStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/RadiiStory.kt
@@ -20,14 +20,23 @@
 package net.skyscanner.backpack.demo.compose
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import net.skyscanner.backpack.compose.text.BpkText
+import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
+import net.skyscanner.backpack.compose.tokens.BpkColor
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.demo.data.Token
 import net.skyscanner.backpack.demo.data.values
 
@@ -44,14 +53,30 @@ fun RadiiComposeStory() {
 
 @Composable
 private fun RadiiSample(token: Token<Dp>) {
-  Box() {
+  Box(
+    modifier = Modifier.padding(BpkSpacing.Base),
+  ) {
     RadiiCard(token = token)
   }
 }
 
 @Composable
 private fun RadiiCard(token: Token<Dp>) {
-  Card() {
-    BpkText(text = "${token.name} = ${token.value}")
+  Surface(
+    modifier = Modifier.sizeIn(minHeight = token.value * 2),
+    color = BpkColor.SkyBlueShade01,
+    shape = RoundedCornerShape(token.value),
+  ) {
+    Box(
+      contentAlignment = Alignment.Center,
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(BpkSpacing.Base),
+    ) {
+      BpkText(
+        text = "${token.name} = ${token.value}",
+        style = BpkTheme.typography.bodyLongform,
+      )
+    }
   }
 }
