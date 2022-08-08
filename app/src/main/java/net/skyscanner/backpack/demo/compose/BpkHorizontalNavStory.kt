15fc753af7005bba37aecb0264625de915b1d19d
bvitaliyg <bvitaliyg@gmail.com>
2022-08-08 15:55:44 +0100

    Updated the samples.

--- app/src/main/java/net/skyscanner/backpack/demo/compose/BpkHorizontalNavStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/BpkHorizontalNavStory.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.skyscanner.backpack.compose.horizontalnav.BpkHorizontalNav
 import net.skyscanner.backpack.compose.horizontalnav.BpkHorizontalNavSize
@@ -38,8 +39,8 @@ import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.compose.tokens.CloseCircle
 import net.skyscanner.backpack.compose.tokens.TickCircle
+import net.skyscanner.backpack.demo.R
 
-@Preview
 @Composable
 fun HorizontalNavComposeStory() {
 
@@ -50,28 +51,29 @@ fun HorizontalNavComposeStory() {
     verticalArrangement = Arrangement.spacedBy(BpkSpacing.Base),
   ) {
 
-    BpkText(text = "Small")
+    BpkText(text = stringResource(R.string.tabs_small))
     BpkHorizontalNav_Small_Sample()
 
-    BpkText(text = "Small with icon")
+    BpkText(text = stringResource(R.string.tabs_small_with_icon))
     BpkHorizontalNav_SmallWithIcon_Sample()
 
-    BpkText(text = "Large")
+    BpkText(text = stringResource(R.string.tabs_large))
     BpkHorizontalNav_Large_Sample()
 
-    BpkText(text = "Large with icon")
+    BpkText(text = stringResource(R.string.tabs_large_with_icon))
     BpkHorizontalNav_LargeWithIcon_Sample()
   }
 }
 
+@Preview
 @Composable
 internal fun BpkHorizontalNav_Small_Sample() {
   val tabs = listOf(
     BpkHorizontalNavTab(
-      title = "One",
+      title = stringResource(R.string.tabs_one),
     ),
     BpkHorizontalNavTab(
-      title = "Two",
+      title = stringResource(R.string.tabs_two),
     ),
   )
 
@@ -85,15 +87,16 @@ internal fun BpkHorizontalNav_Small_Sample() {
   )
 }
 
+@Preview
 @Composable
 internal fun BpkHorizontalNav_SmallWithIcon_Sample() {
   val tabs = listOf(
     BpkHorizontalNavTab(
-      title = "One",
+      title = stringResource(R.string.tabs_one),
       icon = BpkIcon.TickCircle,
     ),
     BpkHorizontalNavTab(
-      title = "Two",
+      title = stringResource(R.string.tabs_two),
       icon = BpkIcon.CloseCircle,
     ),
   )
@@ -108,14 +111,15 @@ internal fun BpkHorizontalNav_SmallWithIcon_Sample() {
   )
 }
 
+@Preview
 @Composable
 internal fun BpkHorizontalNav_Large_Sample() {
   val tabs = listOf(
     BpkHorizontalNavTab(
-      title = "One",
+      title = stringResource(R.string.tabs_one),
     ),
     BpkHorizontalNavTab(
-      title = "Two",
+      title = stringResource(R.string.tabs_two),
     ),
   )
 
@@ -128,15 +132,16 @@ internal fun BpkHorizontalNav_Large_Sample() {
   )
 }
 
+@Preview
 @Composable
 internal fun BpkHorizontalNav_LargeWithIcon_Sample() {
   val tabs = listOf(
     BpkHorizontalNavTab(
-      title = "One",
+      title = stringResource(R.string.tabs_one),
       icon = BpkIcon.TickCircle,
     ),
     BpkHorizontalNavTab(
-      title = "Two",
+      title = stringResource(R.string.tabs_two),
       icon = BpkIcon.CloseCircle,
     ),
   )
