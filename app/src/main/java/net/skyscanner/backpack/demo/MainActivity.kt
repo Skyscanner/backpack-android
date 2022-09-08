6cc5896c529ca66e44675cb54c05276a7123fd7f
jaanainkinen <63608778+jaanainkinen@users.noreply.github.com>
2022-09-08 13:52:52 +0100

    Converted the component list screen to Compose (#1197)
    * Add components and Compose badges
    
    * Adjust padding and fix scrollability
    
    * Fix scrollability and paddings
    
    * Refactor code
    
    * Remove commented code
    
    * Review comments in PR
    
    * Make ComponentScreen function private
    
    * Change Backpack theme into BackpackDemoTheme

--- app/src/main/java/net/skyscanner/backpack/demo/MainActivity.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/MainActivity.kt
@@ -18,12 +18,34 @@
 
 package net.skyscanner.backpack.demo
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.RecyclerView
-import net.skyscanner.backpack.demo.StoriesRecyclerViewAdapter.HeaderItem
-import net.skyscanner.backpack.demo.StoriesRecyclerViewAdapter.StoryItem
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.badge.BpkBadge
+import net.skyscanner.backpack.compose.badge.BpkBadgeType
+import net.skyscanner.backpack.compose.icon.BpkIcon
+import net.skyscanner.backpack.compose.navigationbar.BpkTopNavBar
+import net.skyscanner.backpack.compose.navigationbar.IconAction
+import net.skyscanner.backpack.compose.navigationbar.NavIcon
+import net.skyscanner.backpack.compose.text.BpkText
+import net.skyscanner.backpack.compose.theme.BpkTheme
+import net.skyscanner.backpack.compose.tokens.BpkDimension
+import net.skyscanner.backpack.compose.tokens.Settings
 import net.skyscanner.backpack.demo.data.ComponentRegistry
 import net.skyscanner.backpack.demo.data.ComposeNode
 import net.skyscanner.backpack.demo.data.NodeItem
@@ -41,25 +63,97 @@ class MainActivity : BpkBaseActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_component_list)
-
-    val toolbar = findViewById<Toolbar>(R.id.toolbar)
-    setSupportActionBar(toolbar)
-    toolbar.title = title
-
-    val componentsList = findViewById<View>(R.id.componentsList) as RecyclerView
-    val allItems = mutableListOf<StoriesRecyclerViewAdapter.ListItem>()
-    allItems.add(HeaderItem("Tokens"))
-    allItems.addAll(ComponentRegistry.TOKENS.map { it.toStoryItem() })
-    allItems.add(HeaderItem("Components"))
-    allItems.addAll(ComponentRegistry.COMPONENTS.map { it.toStoryItem() })
-
-    componentsList.adapter = StoriesRecyclerViewAdapter(allItems)
-    componentsList.addItemDecoration(StoryItemDecoration(this))
+    setContent {
+      BackpackDemoTheme {
+        ComponentScreen()
+      }
+    }
   }
 
-  private fun Map.Entry<String, NodeItem>.toStoryItem(): StoryItem {
-    return StoryItem(key, hasComposeNodes(value))
+  @Composable
+  private fun ComponentScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+      val context = LocalContext.current
+      BpkTopNavBar(
+        navIcon = NavIcon.None,
+        title = stringResource(R.string.app_name),
+        actions = listOf(
+          IconAction(
+            icon = BpkIcon.Settings,
+            contentDescription = stringResource(R.string.settings_title),
+            onClick = {
+              val intent = Intent(context, SettingsActivity::class.java)
+              context.startActivity(intent)
+            }
+          )
+        )
+      )
+      LazyColumn {
+        item {
+          ComponentsTitle(stringResource(R.string.tokens_title))
+        }
+        items(ComponentRegistry.TOKENS.values.toList()) {
+          ComponentItem(title = it.name, showComposeBadge = hasComposeNodes(item = it))
+        }
+        item {
+          ComponentsTitle(title = stringResource(R.string.components_title))
+        }
+        items(ComponentRegistry.COMPONENTS.values.toList()) {
+          ComponentItem(title = it.name, showComposeBadge = hasComposeNodes(item = it))
+        }
+      }
+    }
+  }
+
+  @Composable
+  fun ComponentItem(
+    title: String,
+    showComposeBadge: Boolean,
+    modifier: Modifier = Modifier,
+  ) {
+    val context = LocalContext.current
+    Column(
+      modifier = modifier
+        .clickable {
+          val intent = Intent(context, ComponentDetailActivity::class.java)
+          intent.putExtra(ComponentDetailFragment.ARG_ITEM_ID, title)
+          context.startActivity(intent)
+        }
+        .height(56.dp)
+        .fillMaxWidth()
+        .padding(horizontal = BpkDimension.Spacing.Lg),
+    ) {
+
+      Row(
+        modifier = Modifier.weight(1f),
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        BpkText(text = title, style = BpkTheme.typography.bodyLongform)
+        if (showComposeBadge) {
+          BpkBadge(
+            text = stringResource(R.string.story_badge_compose),
+            type = BpkBadgeType.Success,
+            modifier = Modifier.padding(start = BpkDimension.Spacing.Base)
+          )
+        }
+      }
+      Divider()
+    }
+  }
+
+  @Composable
+  fun ComponentsTitle(title: String, modifier: Modifier = Modifier) {
+    BpkText(
+      text = title.uppercase(),
+      modifier = modifier
+        .fillMaxWidth()
+        .padding(
+          vertical = BpkDimension.Spacing.Base,
+          horizontal = BpkDimension.Spacing.Lg
+        ),
+      color = BpkTheme.colors.textSecondary,
+      style = BpkTheme.typography.label2,
+    )
   }
 
   private fun hasComposeNodes(item: RegistryItem): Boolean {
