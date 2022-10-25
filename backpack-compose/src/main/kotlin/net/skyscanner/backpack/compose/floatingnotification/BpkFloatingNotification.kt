946524eba0ba6b89c47a3724a533950ecf33665d
Mahdi Sadeghi <mahdi.sadeghi@skyscanner.net>
2022-10-25 14:20:47 +0100

    SONIC-FloatingNotificationCompose remove previews

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/floatingnotification/BpkFloatingNotification.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/floatingnotification/BpkFloatingNotification.kt
@@ -18,7 +18,6 @@
 
 package net.skyscanner.backpack.compose.floatingnotification
 
-import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
@@ -38,7 +37,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,10 +46,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import net.skyscanner.backpack.compose.icon.BpkIcon
@@ -60,7 +55,6 @@ import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
-import net.skyscanner.backpack.compose.tokens.Heart
 import net.skyscanner.backpack.compose.utils.clickable
 
 @Composable
@@ -168,105 +162,3 @@ data class Animation(
   val animateOnExit: Boolean = true,
   val hideAfter: Int = 4000,
 )
-
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun LightMode_TextOnly() {
-  // run in Interactive Mode
-  val (show, setShow) = remember { mutableStateOf(false) }
-  BpkFloatingNotification(
-    text = "Lorem",
-    show = show
-  )
-  LaunchedEffect(key1 = Unit) {
-    delay(1000)
-    setShow(true)
-  }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun LightMode_Icon() {
-  val (show, setShow) = remember { mutableStateOf(false) }
-  BpkFloatingNotification(
-    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    icon = BpkIcon.Heart,
-    show = show
-  )
-  LaunchedEffect(key1 = Unit) {
-    delay(1000)
-    setShow(true)
-  }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun LightMode_CTA() {
-  val (show, setShow) = remember { mutableStateOf(false) }
-  BpkFloatingNotification(
-    text = "Lorem ipsum dolor sit amet sdfs fsfsd fsdfsdf dsdfs fsdfs",
-    icon = BpkIcon.Heart,
-    cta = Cta("open whatever", onClick = {}),
-    show = show
-  )
-  LaunchedEffect(key1 = Unit) {
-    delay(1000)
-    setShow(true)
-  }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun LightMode_CTA_NoAnimation() {
-  val (show, setShow) = remember { mutableStateOf(false) }
-  BpkFloatingNotification(
-    text = "Lorem ipsum dolor sit amet",
-    icon = BpkIcon.Heart,
-    cta = Cta("View", onClick = {}),
-    animation = Animation(
-      animateOnEnter = false,
-      animateOnExit = false
-    ),
-    show = show
-  )
-  LaunchedEffect(key1 = Unit) {
-    delay(1000)
-    setShow(true)
-  }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, backgroundColor = 0x000000)
-@Composable
-private fun NightMode_CTA() {
-  val (show, setShow) = remember { mutableStateOf(false) }
-  BpkFloatingNotification(
-    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    icon = BpkIcon.Heart,
-    cta = Cta("Open", onClick = {}),
-    show = show
-  )
-  LaunchedEffect(key1 = Unit) {
-    delay(1000)
-    setShow(true)
-  }
-}
-
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun LightMode_TextOnly_RTL() {
-  CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-    // run in Interactive Mode
-    val (show, setShow) = remember { mutableStateOf(false) }
-    BpkFloatingNotification(
-      text = "خسیر خسی س خسهد ی سخهسیخ عسی سخیه دسخهی د سسخی دس خحهسید سیمسی  ",
-      icon = BpkIcon.Heart,
-      cta = Cta("تماشا", onClick = {}),
-      show = show
-    )
-    LaunchedEffect(key1 = Unit) {
-      delay(1000)
-      setShow(true)
-    }
-  }
-}
