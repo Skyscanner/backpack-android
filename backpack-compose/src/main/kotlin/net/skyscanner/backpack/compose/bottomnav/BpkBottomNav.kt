dbc05c064585d3ea200cd9ff90d6a5ec41271132
Jaana Inkinen <jaanainkinen@skyscanner.net>
2022-11-15 14:50:05 +0000

    Create function for BpkBottomNav

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomnav/BpkBottomNav.kt
@@ -0,0 +1,43 @@
+/*
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.skyscanner.backpack.compose.bottomnav
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import net.skyscanner.backpack.compose.theme.BpkTheme
+
+@Composable
+fun BpkBottomNav(
+  modifier: Modifier = Modifier,
+  elevation: Dp = BottomNavigationDefaults.Elevation,
+  content: @Composable RowScope.() -> Unit
+) {
+  BottomNavigation(
+    modifier = modifier,
+    backgroundColor = BpkTheme.colors.surfaceDefault,
+    contentColor = BpkTheme.colors.textLink,
+    elevation = elevation,
+    content = content,
+  )
+}
