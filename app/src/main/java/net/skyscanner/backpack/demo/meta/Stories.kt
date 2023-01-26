8030d26832e76a0fa6342e6b766e2fd0a9ccf598
bvitaliyg <bvitaliyg@gmail.com>
2023-01-26 09:08:56 +0000

    Added helper composables

--- app/src/main/java/net/skyscanner/backpack/demo/meta/Stories.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/meta/Stories.kt
@@ -1,8 +1,13 @@
 package net.skyscanner.backpack.demo.meta
 
 import android.content.res.Configuration
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.LayoutRes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Suppress("Detekt.PreviewNaming")
 @Target(AnnotationTarget.FUNCTION)
@@ -23,3 +28,35 @@ data class StoryEntry(
   val screenshot: Boolean = true,
   val content: @Composable () -> Unit,
 )
+
+@Composable
+fun <T : View> AndroidLayout(
+  @LayoutRes id: Int,
+  modifier: Modifier = Modifier,
+  update: T.() -> Unit,
+) {
+  AndroidView(
+    modifier = modifier,
+    factory = { LayoutInflater.from(it).inflate(id, null) as T },
+    update = { update(it) },
+  )
+}
+
+@Composable
+fun AndroidLayout(
+  @LayoutRes id: Int,
+  modifier: Modifier = Modifier,
+) =
+  AndroidLayout<View>(id, modifier) {}
+
+@Composable
+inline fun <reified T : View> AndroidView(
+  modifier: Modifier = Modifier,
+  crossinline update: T.() -> Unit,
+) {
+  AndroidView(
+    modifier = modifier,
+    factory = { T::class.java.constructors.first { it.parameters.size == 1 }.newInstance(it) as T },
+    update = { update(it) },
+  )
+}
