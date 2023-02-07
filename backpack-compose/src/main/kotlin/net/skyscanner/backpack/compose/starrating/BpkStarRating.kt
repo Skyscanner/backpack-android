c2084c74353a97e1b9c885bda16930574b9f60ae
Jaana Inkinen <jaanainkinen@skyscanner.net>
2023-02-07 14:18:33 +0000

    Review PR comments

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/starrating/BpkStarRating.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/starrating/BpkStarRating.kt
@@ -34,6 +34,7 @@ import net.skyscanner.backpack.compose.tokens.Star
 import net.skyscanner.backpack.compose.tokens.StarHalf
 import net.skyscanner.backpack.compose.tokens.StarOutline
 import net.skyscanner.backpack.compose.utils.ContentDescriptionScope
+import net.skyscanner.backpack.compose.utils.applyIf
 import net.skyscanner.backpack.compose.utils.clickable
 import net.skyscanner.backpack.compose.utils.rememberContentDescriptionScope
 
@@ -59,14 +60,10 @@ fun BpkStarRating(
   BpkStarRating(
     maxRating = 5,
     rounding = rounding,
-    iconSize = when (size) {
-      BpkStarRatingSize.Large -> BpkIconSize.Large
-      BpkStarRatingSize.Small -> BpkIconSize.Small
-    },
     modifier = modifier,
     rating = rating,
     contentDescription = contentDescription,
-    onRatingSelected = null,
+    size = size,
   )
 }
 
@@ -80,14 +77,10 @@ fun BpkHotelRating(
   BpkStarRating(
     maxRating = rating,
     rounding = BpkRatingRounding.Down,
-    iconSize = when (size) {
-      BpkStarRatingSize.Large -> BpkIconSize.Large
-      BpkStarRatingSize.Small -> BpkIconSize.Small
-    },
     modifier = modifier,
     rating = rating.toFloat(),
     contentDescription = contentDescription,
-    onRatingSelected = null,
+    size = size,
   )
 }
 
@@ -103,14 +96,11 @@ fun BpkInteractiveStarRating(
   BpkStarRating(
     maxRating = 5,
     rounding = rounding,
-    iconSize = when (size) {
-      BpkStarRatingSize.Large -> BpkIconSize.Large
-      BpkStarRatingSize.Small -> BpkIconSize.Small
-    },
     modifier = modifier,
     onRatingSelected = onRatingSelected,
     rating = selectedRating.toFloat(),
     contentDescription = contentDescription,
+    size = size,
   )
 }
 
@@ -119,11 +109,15 @@ private fun BpkStarRating(
   rating: Float,
   maxRating: Int,
   rounding: BpkRatingRounding,
-  iconSize: BpkIconSize,
   contentDescription: ContentDescriptionScope.(Float, Int) -> String,
-  onRatingSelected: ((Int) -> Unit)?,
   modifier: Modifier = Modifier,
+  size: BpkStarRatingSize = BpkStarRatingSize.Small,
+  onRatingSelected: ((Int) -> Unit)? = null,
 ) {
+  val iconSize = when (size) {
+    BpkStarRatingSize.Large -> BpkIconSize.Large
+    BpkStarRatingSize.Small -> BpkIconSize.Small
+  }
   val coercedRating = rating.coerceIn(0f, maxRating.toFloat())
   val roundedRating = when (rounding) {
     BpkRatingRounding.Down -> floor(coercedRating * 2) / 2
@@ -134,7 +128,10 @@ private fun BpkStarRating(
   Row(modifier = modifier.semantics { this.contentDescription = scope.contentDescription(roundedRating, maxRating) }) {
     for (item in 0 until maxRating) {
       val value = (roundedRating - item).coerceIn(0f, 1f)
-      val starModifier = Modifier.clickable { onRatingSelected?.invoke(item + 1) }
+      val starModifier = Modifier
+        .applyIf(onRatingSelected != null) {
+          clickable { onRatingSelected?.invoke(item + 1) }
+        }
       when {
         (value >= 0.0f && value < 0.5f) -> BpkStar(
           icon = BpkRatingStarType.Empty,
