c1ec70274407016831348508a2bfc35f4acadf99
bvitaliyg <bvitaliyg@gmail.com>
2023-02-27 14:52:53 +0000

    Updated compose version of HorizontalNavStory

--- app/src/main/java/net/skyscanner/backpack/demo/compose/HorizontalNavStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/HorizontalNavStory.kt
@@ -40,13 +40,15 @@ import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.compose.tokens.CloseCircle
 import net.skyscanner.backpack.compose.tokens.TickCircle
 import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.demo.components.HorizontalNavComponent
+import net.skyscanner.backpack.demo.meta.ComposeStory
 
 @Composable
-@Preview
-fun HorizontalNavStory() {
-
+@HorizontalNavComponent
+@ComposeStory
+fun HorizontalNavStory(modifier: Modifier = Modifier) {
   Column(
-    modifier = Modifier
+    modifier = modifier
       .background(BpkTheme.colors.surfaceHighlight)
       .padding(BpkSpacing.Base),
     verticalArrangement = Arrangement.spacedBy(BpkSpacing.Base),
