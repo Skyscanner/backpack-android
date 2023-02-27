912e87d98c0ca012117a29d998dd2c8c4561ba70
bvitaliyg <bvitaliyg@gmail.com>
2023-02-27 23:03:43 +0000

    DialogStory update

--- app/src/main/java/net/skyscanner/backpack/demo/stories/DialogStory.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/stories/DialogStory.kt
@@ -22,10 +22,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.core.view.forEach
 import com.squareup.picasso.Picasso
 import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.demo.components.DialogComponent
+import net.skyscanner.backpack.demo.meta.ViewStory
+import net.skyscanner.backpack.demo.ui.AndroidLayout
 import net.skyscanner.backpack.dialog.BpkDialog
+
+@Composable
+@DialogComponent
+@ViewStory
+fun DialogStory(modifier: Modifier = Modifier) =
+  AndroidLayout(R.layout.fragment_dialog, modifier) {
+    findViewById<ViewGroup>(R.id.dialog_root).forEach { buttonView ->
+      buttonView.setOnClickListener {
+        it.handleOnClick(buttonView)
+      }
+    }
+  }
 
 class DialogStory : Story() {
 
@@ -39,169 +56,169 @@ class DialogStory : Story() {
     super.onViewCreated(view, savedInstanceState)
     view.findViewById<ViewGroup>(R.id.dialog_root).forEach { buttonView ->
       buttonView.setOnClickListener {
-        handleOnClick(buttonView)
+        it.handleOnClick(buttonView)
       }
     }
   }
+}
 
-  private fun handleOnClick(view: View) {
-    when (view.id) {
-      R.id.dialog_success_one_button -> successOneButtonDialogExample()
-      R.id.dialog_success_two_buttons -> successTwoButtonsDialogExample()
-      R.id.dialog_success_three_buttons -> successThreeButtonsDialogExample()
-      R.id.dialog_warning -> warningDialogExample()
-      R.id.dialog_destructive -> destructiveDialogExample()
-      R.id.dialog_no_icon -> noIconDialogExample()
-      R.id.dialog_long_text -> longDialogExample()
-      R.id.dialog_flare -> flareDialogExample()
-    }
+private fun View.handleOnClick(view: View) {
+  when (view.id) {
+    R.id.dialog_success_one_button -> successOneButtonDialogExample()
+    R.id.dialog_success_two_buttons -> successTwoButtonsDialogExample()
+    R.id.dialog_success_three_buttons -> successThreeButtonsDialogExample()
+    R.id.dialog_warning -> warningDialogExample()
+    R.id.dialog_destructive -> destructiveDialogExample()
+    R.id.dialog_no_icon -> noIconDialogExample()
+    R.id.dialog_long_text -> longDialogExample()
+    R.id.dialog_flare -> flareDialogExample()
   }
+}
 
-  private fun successOneButtonDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Success).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      icon = BpkDialog.Icon(R.drawable.bpk_tick)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.successOneButtonDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Success).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    icon = BpkDialog.Icon(R.drawable.bpk_tick)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun successTwoButtonsDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Success).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      icon = BpkDialog.Icon(R.drawable.bpk_tick)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.successTwoButtonsDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Success).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    icon = BpkDialog.Icon(R.drawable.bpk_tick)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun successThreeButtonsDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Success).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      icon = BpkDialog.Icon(R.drawable.bpk_tick)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_link_optional)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.successThreeButtonsDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Success).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    icon = BpkDialog.Icon(R.drawable.bpk_tick)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_link_optional)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun warningDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Warning).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      icon = BpkDialog.Icon(R.drawable.bpk_alert__add)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_link_optional)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.warningDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Warning).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    icon = BpkDialog.Icon(R.drawable.bpk_alert__add)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_link_optional)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun destructiveDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Destructive).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      icon = BpkDialog.Icon(R.drawable.bpk_trash)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_delete)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_cancel)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.destructiveDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Destructive).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    icon = BpkDialog.Icon(R.drawable.bpk_trash)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_delete)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_cancel)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun longDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Success).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.stub).repeat(3)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_continue)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.longDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Success).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.stub).repeat(3)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_continue)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun noIconDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Success).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+private fun View.noIconDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Success).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+  }.show()
+}
 
-  private fun flareDialogExample() {
-    BpkDialog(requireContext(), BpkDialog.Type.Flare).apply {
-      title = getString(R.string.dialog_title)
-      description = getString(R.string.dialog_text)
-      Picasso.get().load("file:///android_asset/dialog_sample.jpg").noFade().into(image)
+private fun View.flareDialogExample() {
+  BpkDialog(context, BpkDialog.Type.Flare).apply {
+    title = context.getString(R.string.dialog_title)
+    description = context.getString(R.string.dialog_text)
+    Picasso.get().load("file:///android_asset/dialog_sample.jpg").noFade().into(image)
 
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_confirmation)) {
-          dismiss()
-        },
-      )
-      addActionButton(
-        BpkDialog.Button(getString(R.string.dialog_skip)) {
-          dismiss()
-        },
-      )
-    }.show()
-  }
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_confirmation)) {
+        dismiss()
+      },
+    )
+    addActionButton(
+      BpkDialog.Button(context.getString(R.string.dialog_skip)) {
+        dismiss()
+      },
+    )
+  }.show()
 }
