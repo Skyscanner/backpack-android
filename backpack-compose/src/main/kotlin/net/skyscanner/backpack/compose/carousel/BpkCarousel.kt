cb56fdc1c5c9000def799dcd7b6542d594c83a86
Mines Chan <mineschan@gmail.com>
2023-02-28 15:13:06 +0000

    Added BpkCarousel (#1468, Skyscanner/sonic 1203)
    * Add BpkCarousel
    
    * Add doc and Test
    
    * Add screenshots to Carousel doc
    
    * Updated snapshots for 'rtl'
    
    * Update resource in BpkCarouselTest
    
    * Trigger CI
    
    * Resource relocation
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'default'
    
    * Add accompanistVersion in gradle
    
    * Update demo title with correct heading and regenerate the screenshots
    
    * Updated snapshots for 'dm'
    
    * Hoist the PagerState, remove Saveable
    
    * Remove currentImage parameters
    
    * Update screen shots and Readme
    
    * Record snapshots
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'default'
    
    * Rename imageContent and fix single image carousel
    
    * Refactor with custom BpkCarouselState delegates PagerState
    
    * Add BpkCarouselInternalState
    
    * Updated snapshots for 'dm'
    
    * Updated snapshots for 'default'
    
    * Move getModdedPageNumber to BpkCarouselState
    
    * Remove internal interfaces
    
    * Update Readme
    
    * Updated snapshots for 'rtl'
    
    * Minor Update
    
    * Record snapshots
    
    * Minor Updates
    
    * Remove unused state in Story
    
    ---------
    
    Co-authored-by: Mines Chan <mines.chan@skyscanner.net>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/carousel/BpkCarousel.kt
@@ -0,0 +1,64 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.carousel
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import net.skyscanner.backpack.compose.pageindicator.BpkPageIndicator
+import net.skyscanner.backpack.compose.pageindicator.BpkPageIndicatorStyle
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun BpkCarousel(
+  state: BpkCarouselState,
+  modifier: Modifier = Modifier,
+  content: @Composable (BoxScope.(Int) -> Unit),
+) {
+  val internalState = state.asInternalState()
+  Box(modifier = modifier) {
+    HorizontalPager(
+      modifier = Modifier
+        .testTag("pager")
+        .fillMaxSize(),
+      count = if (internalState.pageCount > 1) Int.MAX_VALUE else 1, // if count > 1, set to Int.MAX_VALUE for infinite looping
+      state = internalState.delegate,
+    ) {
+      content(internalState.currentPage)
+    }
+
+    // if there is more than one image, display the page indicator
+    if (internalState.pageCount > 1) {
+      BpkPageIndicator(
+        modifier = Modifier
+          .align(Alignment.BottomCenter)
+          .testTag("pageIndicator"),
+        totalIndicators = internalState.pageCount,
+        currentIndex = internalState.currentPage,
+        style = BpkPageIndicatorStyle.OverImage,
+      )
+    }
+  }
+}
