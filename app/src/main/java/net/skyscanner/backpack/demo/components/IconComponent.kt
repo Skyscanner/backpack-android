7a80dab85b7d4a5c950270d8a9514fec2a3cdec0
bvitaliyg <bvitaliyg@gmail.com>
2023-02-28 12:30:35 +0000

    IconComponent

--- app/src/main/java/net/skyscanner/backpack/demo/components/IconComponent.kt
+++ app/src/main/java/net/skyscanner/backpack/demo/components/IconComponent.kt
@@ -2,5 +2,5 @@ package net.skyscanner.backpack.demo.components
 
 import net.skyscanner.backpack.demo.meta.Component
 
-@Component("Icon")
+@Component("All icons", isToken = true)
 annotation class IconComponent
