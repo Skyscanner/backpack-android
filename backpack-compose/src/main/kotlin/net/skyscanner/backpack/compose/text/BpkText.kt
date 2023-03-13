6c322bda52de7a40d69794a65c3c82f6062fb62b
bvitaliyg <bvitaliyg@gmail.com>
2023-03-13 14:48:37 +0000

    BpkText

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/text/BpkText.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/text/BpkText.kt
@@ -18,8 +18,8 @@
 
 package net.skyscanner.backpack.compose.text
 
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Text
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
