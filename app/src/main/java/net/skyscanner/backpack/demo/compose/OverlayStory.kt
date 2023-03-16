d42034b9d7ebc12752886f3044f166a97d6fbbf2
jaanainkinen <63608778+jaanainkinen@users.noreply.github.com>
2023-03-16 13:18:17 +0000

    Koa-4925 Create BpkOverlay in Compose (#1498)
    * Create files for BpkOverlay
    
    * Create component BpkOverlay
    
    * Create story for BpkOverlay
    
    * Add new image for BpkOverlay samples
    
    * Implement RTL
    
    * Tweak RTL implementation
    
    * Simplify class name
    
    * Shorten if statements
    
    * Rearrange arguments on separate lines
    
    * Add new image description
    
    * Change modifier size
    
    * Change image dimensions
    
    * Change text colour in foregroundContent
    
    * Add screenshots for BpkOverlay
    
    * Add readme for BpkOverlay
    
    * Add tests for BpkOverlay
    
    * Fix readme
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'dm'
    
    * Updated snapshots for 'default'
    
    * Trigger CI
    
    * Trigger CI
    
    * Review PR comments
    
    * Generate screenshots
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'default'
    
    * Review PR comments
    
    * Updated snapshots for 'dm'
    
    * Use parametrised tests
    
    * Updated snapshots for 'rtl'
    
    * Trigger CI
    
    * Trigger CI
    
    * Updated snapshots for 'dm'
    
    * Trigger CI
    
    * Trigger CI
    
    * Updated snapshots for 'default'
    
    * Review comments
    
    * Trigger CI
    
    * Move to manual drawing
    
    * Implement vignette drawing
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'dm'
    
    * Updated snapshots for 'default'
    
    * Record new screenshots
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/OverlayStory.kt
@@ -0,0 +1,161 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.demo.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import net.skyscanner.backpack.compose.overlay.BpkOverlay
+import net.skyscanner.backpack.compose.overlay.BpkOverlayType
+import net.skyscanner.backpack.compose.text.BpkText
+import net.skyscanner.backpack.compose.theme.BpkTheme
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
+import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.demo.components.OverlayComponent
+import net.skyscanner.backpack.demo.meta.ComposeStory
+
+@Composable
+@OverlayComponent
+@ComposeStory
+fun OverlayStory(modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier
+      .verticalScroll(rememberScrollState())
+      .padding(BpkSpacing.Base),
+    verticalArrangement = Arrangement.spacedBy(BpkSpacing.Base),
+  ) {
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.SolidLow, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.SolidMedium, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.SolidHigh, modifier = childModifier)
+    }
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.TopLow, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.TopMedium, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.TopHigh, modifier = childModifier)
+    }
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.BottomLow, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.BottomMedium, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.BottomHigh, modifier = childModifier)
+    }
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.LeftLow, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.LeftMedium, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.LeftHigh, modifier = childModifier)
+    }
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.RightLow, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.RightMedium, modifier = childModifier)
+      DefaultOverlaySample(overlayType = BpkOverlayType.RightHigh, modifier = childModifier)
+    }
+    OverlaySampleRow {
+      val childModifier = Modifier.weight(1f)
+      DefaultOverlaySample(overlayType = BpkOverlayType.Vignette, modifier = childModifier)
+      WithTextOverlaySample(overlayType = BpkOverlayType.SolidHigh, modifier = childModifier)
+      Spacer(modifier = childModifier)
+    }
+  }
+}
+
+@Composable
+fun OverlaySampleRow(
+  modifier: Modifier = Modifier,
+  content: @Composable RowScope.() -> Unit,
+) {
+  Row(
+    content = content,
+    horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Base),
+    modifier = modifier.fillMaxWidth(),
+  )
+}
+
+@Composable
+fun DefaultOverlaySample(
+  overlayType: BpkOverlayType,
+  modifier: Modifier = Modifier,
+) {
+  Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    BpkOverlay(
+      modifier = Modifier
+        .fillMaxWidth()
+        .aspectRatio(1.5f),
+      overlayType = overlayType,
+    ) {
+      Image(
+        painter = painterResource(R.drawable.sea),
+        contentDescription = stringResource(R.string.image_sea_content_description),
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+      )
+    }
+    BpkText(text = overlayType.toString())
+  }
+}
+
+@Composable
+fun WithTextOverlaySample(
+  overlayType: BpkOverlayType,
+  modifier: Modifier = Modifier,
+) {
+  Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    BpkOverlay(
+      modifier = Modifier
+        .fillMaxWidth()
+        .aspectRatio(1.5f),
+      overlayType = overlayType,
+      foregroundContent = {
+        BpkText(
+          modifier = Modifier.align(Alignment.Center),
+          text = "With text",
+          color = BpkTheme.colors.textOnDark,
+          style = BpkTheme.typography.bodyDefault,
+        )
+      },
+    ) {
+      Image(
+        painter = painterResource(R.drawable.sea),
+        contentDescription = stringResource(R.string.image_sea_content_description),
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+      )
+    }
+    BpkText(text = overlayType.toString())
+  }
+}
---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/overlay/BpkOverlay.kt
@@ -0,0 +1,185 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.overlay
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.isUnspecified
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.clipRect
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.unit.LayoutDirection
+import kotlin.math.hypot
+import net.skyscanner.backpack.compose.theme.BpkTheme
+
+enum class BpkOverlayType {
+  SolidLow,
+  SolidMedium,
+  SolidHigh,
+  TopLow,
+  TopMedium,
+  TopHigh,
+  BottomLow,
+  BottomMedium,
+  BottomHigh,
+  LeftLow,
+  LeftMedium,
+  LeftHigh,
+  RightLow,
+  RightMedium,
+  RightHigh,
+  Vignette,
+}
+
+@Composable
+fun BpkOverlay(
+  modifier: Modifier = Modifier,
+  overlayType: BpkOverlayType = BpkOverlayType.SolidHigh,
+  foregroundContent: (@Composable BoxScope.() -> Unit)? = null,
+  content: @Composable BoxScope.() -> Unit,
+) {
+  Box(
+    modifier = modifier,
+  ) {
+    content()
+    Overlay(overlayType = overlayType)
+    if (foregroundContent != null) {
+      foregroundContent()
+    }
+  }
+}
+
+private enum class GradientLevel {
+  Low,
+  Medium,
+  High,
+  Vignette,
+}
+
+private enum class GradientDirection {
+  Solid,
+  Top,
+  Bottom,
+  Left,
+  Right,
+  Vignette,
+}
+
+private fun BpkOverlayType.toGradientlevel(): GradientLevel =
+  when (this) {
+    BpkOverlayType.SolidLow,
+    BpkOverlayType.TopLow,
+    BpkOverlayType.BottomLow,
+    BpkOverlayType.LeftLow,
+    BpkOverlayType.RightLow,
+    -> GradientLevel.Low
+    BpkOverlayType.SolidMedium,
+    BpkOverlayType.TopMedium,
+    BpkOverlayType.BottomMedium,
+    BpkOverlayType.LeftMedium,
+    BpkOverlayType.RightMedium,
+    -> GradientLevel.Medium
+    BpkOverlayType.SolidHigh,
+    BpkOverlayType.TopHigh,
+    BpkOverlayType.BottomHigh,
+    BpkOverlayType.LeftHigh,
+    BpkOverlayType.RightHigh,
+    -> GradientLevel.High
+    BpkOverlayType.Vignette -> GradientLevel.Vignette
+  }
+
+private fun BpkOverlayType.toDirection(layoutDirection: LayoutDirection): GradientDirection =
+  when (this) {
+    BpkOverlayType.SolidLow,
+    BpkOverlayType.SolidMedium,
+    BpkOverlayType.SolidHigh,
+    -> GradientDirection.Solid
+    BpkOverlayType.TopLow,
+    BpkOverlayType.TopMedium,
+    BpkOverlayType.TopHigh,
+    -> GradientDirection.Top
+    BpkOverlayType.BottomLow,
+    BpkOverlayType.BottomMedium,
+    BpkOverlayType.BottomHigh,
+    -> GradientDirection.Bottom
+    BpkOverlayType.LeftLow,
+    BpkOverlayType.LeftMedium,
+    BpkOverlayType.LeftHigh,
+    -> if (layoutDirection == LayoutDirection.Ltr) GradientDirection.Left else GradientDirection.Right
+    BpkOverlayType.RightLow,
+    BpkOverlayType.RightMedium,
+    BpkOverlayType.RightHigh,
+    -> if (layoutDirection == LayoutDirection.Ltr) GradientDirection.Right else GradientDirection.Left
+    BpkOverlayType.Vignette -> GradientDirection.Vignette
+  }
+
+@Composable
+private fun BoxScope.Overlay(
+  overlayType: BpkOverlayType,
+  modifier: Modifier = Modifier,
+) {
+  val gradientColor = BpkTheme.colors.textOnLight
+  Box(
+    modifier = modifier
+      .matchParentSize()
+      .drawWithCache {
+        val direction = overlayType.toDirection(layoutDirection)
+        val level = overlayType.toGradientlevel()
+        val brush = direction.toBrush(gradientLevel = level, color = gradientColor)
+        onDrawBehind {
+          if (overlayType == BpkOverlayType.Vignette) {
+            if (size.isUnspecified || size.isEmpty()) {
+              return@onDrawBehind
+            }
+            val diagonal = hypot(x = size.width, y = size.height)
+            val scaleFactor = diagonal / size.minDimension
+            clipRect {
+              scale(scaleFactor) {
+                drawRect(brush)
+              }
+            }
+          } else {
+            drawRect(brush)
+          }
+        }
+      },
+  )
+}
+
+private fun GradientLevel.toColor(color: Color): Color =
+  when (this) {
+    GradientLevel.Low -> color.copy(alpha = 0.15f)
+    GradientLevel.Medium -> color.copy(alpha = 0.3f)
+    GradientLevel.High -> color.copy(alpha = 0.45f)
+    GradientLevel.Vignette -> color.copy(alpha = 0.06f)
+  }
+
+private fun GradientDirection.toBrush(gradientLevel: GradientLevel, color: Color): Brush =
+  when (this) {
+    GradientDirection.Solid -> Brush.verticalGradient(listOf(gradientLevel.toColor(color), gradientLevel.toColor(color)))
+    GradientDirection.Top -> Brush.verticalGradient(listOf(gradientLevel.toColor(color), Color.Transparent))
+    GradientDirection.Bottom -> Brush.verticalGradient(listOf(Color.Transparent, gradientLevel.toColor(color)))
+    GradientDirection.Left -> Brush.horizontalGradient(listOf(gradientLevel.toColor(color), Color.Transparent))
+    GradientDirection.Right -> Brush.horizontalGradient(listOf(Color.Transparent, gradientLevel.toColor(color)))
+    GradientDirection.Vignette -> Brush.radialGradient(listOf(Color.Transparent, gradientLevel.toColor(color)))
+  }
