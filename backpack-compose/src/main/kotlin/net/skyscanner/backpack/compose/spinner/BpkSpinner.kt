10c26ccb8e235573b1536b22cdcbe7e64c2743c6
bvitaliyg <bvitaliyg@gmail.com>
2023-04-13 15:38:21 +0100

    Updated the spinner

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/spinner/BpkSpinner.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/spinner/BpkSpinner.kt
@@ -19,7 +19,7 @@
 package net.skyscanner.backpack.compose.spinner
 
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
