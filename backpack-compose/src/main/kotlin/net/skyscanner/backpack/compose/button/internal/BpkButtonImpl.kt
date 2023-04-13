f50f42dc5586647a2c3467fb6963668eb2842c6b
bvitaliyg <bvitaliyg@gmail.com>
2023-04-13 16:08:03 +0100

    Reproducible issue

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/button/internal/BpkButtonImpl.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/button/internal/BpkButtonImpl.kt
@@ -31,8 +31,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
@@ -74,16 +74,19 @@ internal fun BpkButtonImpl(
                 .requiredHeight(size.minHeight),
             interactionSource = interactionSource,
             colors = ButtonDefaults.buttonColors(
-                backgroundColor = type.backgroundColor(interactionSource),
+                containerColor = type.backgroundColor(interactionSource),
                 contentColor = type.contentColor(interactionSource),
-                disabledBackgroundColor = if (loading) type.loadingBackgroundColor() else type.disabledBackgroundColor(),
+                disabledContainerColor = if (loading) type.loadingBackgroundColor() else type.disabledBackgroundColor(),
                 disabledContentColor = if (loading) type.loadingContentColor() else type.disabledContentColor(),
             ),
             shape = ButtonShape,
             contentPadding = type.contentPadding,
             elevation = null,
             content = {
-                CompositionLocalProvider(LocalTextStyle provides size.textStyle()) {
+                CompositionLocalProvider(
+                    LocalTextStyle provides size.textStyle(),
+                    LocalContentColor provides androidx.compose.material3.LocalContentColor.current,
+                ) {
                     Box {
                         Row(
                             modifier = Modifier.hideContentIf(loading),
