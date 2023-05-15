2cba8441c7c7581d9b3c12f7828ab1cd8283f239
bvitaliyg <bvitaliyg@gmail.com>
2023-05-15 16:25:20 +0100

    Simplified Tab

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/horizontalnav/BpkHorizontalNav.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/horizontalnav/BpkHorizontalNav.kt
@@ -26,20 +26,15 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.LocalContentColor
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Surface
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
@@ -53,23 +48,13 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.LastBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import net.skyscanner.backpack.compose.icon.BpkIcon
 import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
-import kotlin.math.max
 
 enum class BpkHorizontalNavSize {
     Default,
@@ -104,30 +89,23 @@ fun BpkHorizontalNav(
                 selected = index == activeIndex,
                 modifier = Modifier.weight(1f),
                 onClick = { onChanged(index) },
-                selectedContentColor = BpkTheme.colors.textLink,
-                unselectedContentColor = BpkTheme.colors.textPrimary,
-                text = {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Md),
-                    ) {
-                        if (tab.icon != null) {
-                            BpkIcon(
-                                icon = tab.icon,
-                                contentDescription = null,
-                            )
-                        }
+            ) {
 
-                        BpkText(
-                            text = tab.title,
-                            style = when (size) {
-                                BpkHorizontalNavSize.Default -> BpkTheme.typography.label1
-                                BpkHorizontalNavSize.Small -> BpkTheme.typography.label2
-                            },
-                        )
-                    }
-                },
-            )
+                if (tab.icon != null) {
+                    BpkIcon(
+                        icon = tab.icon,
+                        contentDescription = null,
+                    )
+                }
+
+                BpkText(
+                    text = tab.title,
+                    style = when (size) {
+                        BpkHorizontalNavSize.Default -> BpkTheme.typography.label1
+                        BpkHorizontalNavSize.Small -> BpkTheme.typography.label2
+                    },
+                )
+            }
         }
     }
 }
@@ -198,22 +176,13 @@ private fun Tab(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    text: @Composable (() -> Unit)? = null,
-    icon: @Composable (() -> Unit)? = null,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    selectedContentColor: Color = LocalContentColor.current,
-    unselectedContentColor: Color = selectedContentColor.copy(alpha = ContentAlpha.medium),
+    content: @Composable RowScope.() -> Unit,
 ) {
-    val styledText: @Composable (() -> Unit)? = text?.let {
-        @Composable {
-            val style = MaterialTheme.typography.button.copy(textAlign = TextAlign.Center)
-            ProvideTextStyle(style, content = text)
-        }
-    }
     // The color of the Ripple should always the selected color, as we want to show the color
     // before the item is considered selected, and hence before the new contentColor is
     // provided by TabTransition.
-    val ripple = rememberRipple(bounded = true, color = selectedContentColor)
+    val ripple = rememberRipple(bounded = true, color = BpkTheme.colors.textLink)
     val transition = updateTransition(targetState = selected)
     val color by transition.animateColor(
         transitionSpec = {
@@ -231,14 +200,14 @@ private fun Tab(
             }
         },
         targetValueByState = {
-            if (it) selectedContentColor else unselectedContentColor
+            if (it) BpkTheme.colors.textLink else BpkTheme.colors.textPrimary
         },
     )
     CompositionLocalProvider(
         LocalContentColor provides color.copy(alpha = 1f),
         LocalContentAlpha provides color.alpha,
     ) {
-        Column(
+        Row(
             modifier = modifier
                 .selectable(
                     selected = selected,
@@ -248,143 +217,16 @@ private fun Tab(
                     interactionSource = interactionSource,
                     indication = ripple,
                 )
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            TabBaselineLayout(icon = icon, text = styledText)
-        }
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Md, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            content = content,
+        )
     }
 }
-
-@Composable
-private fun TabBaselineLayout(
-    text: @Composable (() -> Unit)?,
-    icon: @Composable (() -> Unit)?,
-) {
-    Layout(
-        {
-            if (text != null) {
-                Box(
-                    Modifier
-                        .layoutId("text")
-                        .padding(horizontal = HorizontalTextPadding),
-                ) { text() }
-            }
-            if (icon != null) {
-                Box(Modifier.layoutId("icon")) { icon() }
-            }
-        },
-    ) { measurables, constraints ->
-        val textPlaceable = text?.let {
-            measurables.first { it.layoutId == "text" }.measure(
-                // Measure with loose constraints for height as we don't want the text to take up more
-                // space than it needs
-                constraints.copy(minHeight = 0),
-            )
-        }
-
-        val iconPlaceable = icon?.let {
-            measurables.first { it.layoutId == "icon" }.measure(constraints)
-        }
-
-        val tabWidth = max(textPlaceable?.width ?: 0, iconPlaceable?.width ?: 0)
-
-        val tabHeight = if (textPlaceable != null && iconPlaceable != null) {
-            LargeTabHeight
-        } else {
-            SmallTabHeight
-        }.roundToPx()
-
-        val firstBaseline = textPlaceable?.get(FirstBaseline)
-        val lastBaseline = textPlaceable?.get(LastBaseline)
-
-        layout(tabWidth, tabHeight) {
-            when {
-                textPlaceable != null && iconPlaceable != null -> placeTextAndIcon(
-                    density = this@Layout,
-                    textPlaceable = textPlaceable,
-                    iconPlaceable = iconPlaceable,
-                    tabWidth = tabWidth,
-                    tabHeight = tabHeight,
-                    firstBaseline = firstBaseline!!,
-                    lastBaseline = lastBaseline!!,
-                )
-
-                textPlaceable != null -> placeTextOrIcon(textPlaceable, tabHeight)
-                iconPlaceable != null -> placeTextOrIcon(iconPlaceable, tabHeight)
-                else -> {
-                }
-            }
-        }
-    }
-}
-
-private fun Placeable.PlacementScope.placeTextOrIcon(
-    textOrIconPlaceable: Placeable,
-    tabHeight: Int,
-) {
-    val contentY = (tabHeight - textOrIconPlaceable.height) / 2
-    textOrIconPlaceable.placeRelative(0, contentY)
-}
-
-private fun Placeable.PlacementScope.placeTextAndIcon(
-    density: Density,
-    textPlaceable: Placeable,
-    iconPlaceable: Placeable,
-    tabWidth: Int,
-    tabHeight: Int,
-    firstBaseline: Int,
-    lastBaseline: Int,
-) {
-    val baselineOffset = if (firstBaseline == lastBaseline) {
-        SingleLineTextBaselineWithIcon
-    } else {
-        DoubleLineTextBaselineWithIcon
-    }
-
-    // Total offset between the last text baseline and the bottom of the Tab layout
-    val textOffset = with(density) {
-        baselineOffset.roundToPx() + 2.dp.roundToPx()
-    }
-
-    // How much space there is between the top of the icon (essentially the top of this layout)
-    // and the top of the text layout's bounding box (not baseline)
-    val iconOffset = with(density) {
-        iconPlaceable.height + IconDistanceFromBaseline.roundToPx() - firstBaseline
-    }
-
-    val textPlaceableX = (tabWidth - textPlaceable.width) / 2
-    val textPlaceableY = tabHeight - lastBaseline - textOffset
-    textPlaceable.placeRelative(textPlaceableX, textPlaceableY)
-
-    val iconPlaceableX = (tabWidth - iconPlaceable.width) / 2
-    val iconPlaceableY = textPlaceableY - iconOffset
-    iconPlaceable.placeRelative(iconPlaceableX, iconPlaceableY)
-}
-
-// Tab specifications
-private val SmallTabHeight = 48.dp
-private val LargeTabHeight = 72.dp
 
 // Tab transition specifications
 private const val TabFadeInAnimationDuration = 150
 private const val TabFadeInAnimationDelay = 100
 private const val TabFadeOutAnimationDuration = 100
-
-// The horizontal padding on the left and right of text
-private val HorizontalTextPadding = 16.dp
-
-// Distance from the top of the indicator to the text baseline when there is one line of text and an
-// icon
-private val SingleLineTextBaselineWithIcon = 14.dp
-
-// Distance from the top of the indicator to the last text baseline when there are two lines of text
-// and an icon
-private val DoubleLineTextBaselineWithIcon = 6.dp
-
-// Distance from the first text baseline to the bottom of the icon in a combined tab
-private val IconDistanceFromBaseline = 20.sp
-
-// Distance from the end of the leading icon to the start of the text
-private val TextDistanceFromLeadingIcon = 8.dp
