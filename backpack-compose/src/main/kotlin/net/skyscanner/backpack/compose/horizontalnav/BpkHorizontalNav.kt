2b860b961d5df1604291dc73bf830297b7a7112c
Vitaliy Babichev <bvitaliyg@gmail.com>
2023-05-17 15:06:41 +0100

    KOA-6015 Migrate horizontal nav to custom implementation – Compose (#1592)
    * Horizontal nav fork
    
    * Inline 1
    
    * Inline 2
    
    * Inline 3
    
    * Re-written TabHost
    
    * Simplified Tab
    
    * Simplified animations
    
    * Switched to MD3
    
    * Some code cleanup
    
    * Some code cleanup
    
    * Some code cleanup
    
    * Fix for DM
    
    * Rtl fix
    
    * Updated snapshots for 'dm'
    
    * Updated snapshots for 'default'
    
    * Review changes
    
    * Updated snapshots for 'rtl'
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/horizontalnav/BpkHorizontalNav.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/horizontalnav/BpkHorizontalNav.kt
@@ -18,16 +18,42 @@
 
 package net.skyscanner.backpack.compose.horizontalnav
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.UiComposable
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.LocalContentColor
 import net.skyscanner.backpack.compose.icon.BpkIcon
 import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.theme.BpkTheme
@@ -52,49 +78,157 @@ fun BpkHorizontalNav(
     size: BpkHorizontalNavSize = BpkHorizontalNavSize.Default,
 ) {
     TabRow(
-        contentColor = BpkTheme.colors.textLink,
-        backgroundColor = BpkTheme.colors.surfaceDefault,
         selectedTabIndex = activeIndex,
-        divider = {
-            if (BpkTheme.colors.isLight) {
-                TabRowDefaults.Divider(color = BpkTheme.colors.line)
-            }
-        },
+        tabs = tabs,
+        onChanged = onChanged,
         modifier = modifier.height(
             when (size) {
                 BpkHorizontalNavSize.Default -> 48.dp
                 BpkHorizontalNavSize.Small -> 36.dp
             },
         ),
-    ) {
-        tabs.forEachIndexed { index, tab ->
-            Tab(
-                selected = index == activeIndex,
-                onClick = { onChanged(index) },
-                selectedContentColor = BpkTheme.colors.textLink,
-                unselectedContentColor = BpkTheme.colors.textPrimary,
-                text = {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Md),
-                    ) {
-                        if (tab.icon != null) {
-                            BpkIcon(
-                                icon = tab.icon,
-                                contentDescription = null,
-                            )
-                        }
-
-                        BpkText(
-                            text = tab.title,
-                            style = when (size) {
-                                BpkHorizontalNavSize.Default -> BpkTheme.typography.label1
-                                BpkHorizontalNavSize.Small -> BpkTheme.typography.label2
-                            },
-                        )
-                    }
-                },
+    ) { tab ->
+        if (tab.icon != null) {
+            BpkIcon(
+                icon = tab.icon,
+                contentDescription = null,
             )
+        }
+        BpkText(
+            text = tab.title,
+            style = when (size) {
+                BpkHorizontalNavSize.Default -> BpkTheme.typography.label1
+                BpkHorizontalNavSize.Small -> BpkTheme.typography.label2
+            },
+        )
+    }
+}
+
+@Composable
+@UiComposable
+private fun TabRow(
+    selectedTabIndex: Int,
+    onChanged: (Int) -> Unit,
+    tabs: List<BpkHorizontalNavTab>,
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.(BpkHorizontalNavTab) -> Unit,
+) {
+    Surface(
+        modifier = modifier.selectableGroup(),
+        color = BpkTheme.colors.surfaceDefault,
+        contentColor = BpkTheme.colors.textLink,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .drawDivider()
+                .drawIndicator(tabs.size, selectedTabIndex),
+        ) {
+            tabs.forEachIndexed { index, tab ->
+                Tab(
+                    selected = index == selectedTabIndex,
+                    modifier = Modifier.weight(1f),
+                    onClick = { onChanged(index) },
+                ) {
+                    content(tab)
+                }
+            }
         }
     }
 }
+
+private fun Modifier.drawDivider(): Modifier =
+    composed {
+        val dividerColor = if (isSystemInDarkTheme()) Color.Transparent else BpkTheme.colors.line
+        val dividerThickness = with(LocalDensity.current) { 1.dp.toPx() }
+        drawBehind {
+            drawRect(
+                color = dividerColor,
+                topLeft = Offset(0f, size.height - dividerThickness),
+                size = Size(size.width, dividerThickness),
+            )
+        }
+    }
+
+private fun Modifier.drawIndicator(tabsCount: Int, selectedTabIndex: Int): Modifier =
+    composed {
+        val indicatorColor = BpkTheme.colors.textLink
+        val indicatorHeight = with(LocalDensity.current) { 2.dp.toPx() }
+
+        val indicatorOffset by animateFloatAsState(
+            targetValue = selectedTabIndex.toFloat(),
+            animationSpec = tween(durationMillis = IndicatorAnimationDuration, easing = FastOutSlowInEasing),
+            label = "HorizontalNav indicator offset",
+        )
+
+        drawBehind {
+            val tabWidth = size.width / tabsCount
+            val left = tabWidth * indicatorOffset
+
+            scale(scaleX = if (layoutDirection == LayoutDirection.Rtl) -1f else 1f, scaleY = 1f) {
+                drawRect(
+                    color = indicatorColor,
+                    topLeft = Offset(left, size.height - indicatorHeight),
+                    size = Size(tabWidth, indicatorHeight),
+                )
+            }
+        }
+    }
+
+@Composable
+private fun Tab(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable RowScope.() -> Unit,
+) {
+    val ripple = rememberRipple(bounded = true, color = BpkTheme.colors.textLink)
+    val transition = updateTransition(targetState = selected, label = "HorizontalNav.Tab transition")
+    val color by transition.animateColor(
+        transitionSpec = {
+            if (false isTransitioningTo true) {
+                tween(
+                    durationMillis = TabFadeInAnimationDuration,
+                    delayMillis = TabFadeInAnimationDelay,
+                    easing = LinearEasing,
+                )
+            } else {
+                tween(
+                    durationMillis = TabFadeOutAnimationDuration,
+                    easing = LinearEasing,
+                )
+            }
+        },
+        targetValueByState = {
+            if (it) BpkTheme.colors.textLink else BpkTheme.colors.textPrimary
+        },
+        label = "HorizontalNav.Tab color",
+    )
+    CompositionLocalProvider(
+        LocalContentColor provides color,
+    ) {
+        Row(
+            modifier = modifier
+                .selectable(
+                    selected = selected,
+                    onClick = onClick,
+                    enabled = enabled,
+                    role = Role.Tab,
+                    interactionSource = interactionSource,
+                    indication = ripple,
+                )
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Md, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            content = content,
+        )
+    }
+}
+
+// Tab transition specifications
+private const val TabFadeInAnimationDuration = 150
+private const val IndicatorAnimationDuration = 250
+private const val TabFadeInAnimationDelay = 100
+private const val TabFadeOutAnimationDuration = 100
