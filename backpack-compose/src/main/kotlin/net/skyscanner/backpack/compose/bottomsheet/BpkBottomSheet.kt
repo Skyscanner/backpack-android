e8f53dede06049546081c1188adaf42f89aaa4bb
bvitaliyg <bvitaliyg@gmail.com>
2023-05-18 15:29:05 +0100

    Prototype of MD3 BottomSheet

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkBottomSheet.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkBottomSheet.kt
@@ -19,51 +19,34 @@
 package net.skyscanner.backpack.compose.bottomsheet
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffoldDefaults
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.semantics.collapse
-import androidx.compose.ui.semantics.expand
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
-import net.skyscanner.backpack.compose.tokens.BpkElevation
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
-import net.skyscanner.backpack.compose.utils.nestedScrollFixedSwipeable
-import kotlin.math.roundToInt
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BpkBottomSheet(
     sheetContent: @Composable ColumnScope.(PaddingValues) -> Unit,
@@ -79,90 +62,31 @@ fun BpkBottomSheet(
         val peekHeightWithHandle = peekHeight + HandleHeight
         val peekHeightPx = with(LocalDensity.current) { peekHeightWithHandle.toPx() }
         var bottomSheetHeight by remember { mutableStateOf(fullHeight) }
-        val progress = state.wrapped.progress
-        val openingPercent = when (progress.to) {
-            BpkBottomSheetValue.Expanded -> (1f - progress.fraction)
-            BpkBottomSheetValue.Collapsed -> progress.fraction
-        }
+//        val progress = state.wrapped.progress
+//        val openingPercent = when (progress.to) {
+//            BpkBottomSheetValue.Expanded -> (1f - progress.fraction)
+//            BpkBottomSheetValue.Collapsed -> progress.fraction
+//        }
 
-        val radius = BpkBorderRadius.Lg * openingPercent
+//        val radius = BpkBorderRadius.Lg * openingPercent
+        val radius = BpkBorderRadius.Lg
 
         content(PaddingValues(bottom = peekHeightWithHandle))
 
-        Surface(
-            modifier = Modifier
-                .bottomSheetSwipeable(state, fullHeight, peekHeightPx, bottomSheetHeight, sheetGesturesEnabled)
-                .bottomSheetSemantics(state, peekHeightPx, bottomSheetHeight)
-                .fillMaxWidth()
-                .requiredHeightIn(min = peekHeightWithHandle)
-                .onGloballyPositioned { bottomSheetHeight = it.size.height.toFloat() }
-                .offset { IntOffset(0, state.wrapped.offset.value.roundToInt()) },
+        ModalBottomSheet(
+            onDismissRequest = {},
+            modifier = Modifier.fillMaxWidth(),
+            sheetState = state.delegate,
             shape = RoundedCornerShape(topStart = radius, topEnd = radius),
-            elevation = BpkElevation.Lg,
-            color = BpkTheme.colors.surfaceElevated,
+            containerColor = BpkTheme.colors.surfaceElevated,
             contentColor = BpkTheme.colors.textPrimary,
-            content = {
-                Box {
-                    BpkBottomSheetHandle(
-                        modifier = Modifier
-                            .align(Alignment.TopCenter)
-                            .alpha(openingPercent),
-                    )
-                    Column {
-                        sheetContent(PaddingValues(top = HandleHeight * openingPercent))
-                    }
-                }
-            },
+            tonalElevation = 0.dp,
+            scrimColor = Color.Transparent,
+            dragHandle = { BpkBottomSheetHandle() },
+            content = { sheetContent(PaddingValues.Absolute(0.dp)) },
         )
     }
 }
-
-@OptIn(ExperimentalMaterialApi::class)
-private fun Modifier.bottomSheetSwipeable(
-    state: BpkBottomSheetState,
-    fullHeight: Float,
-    peekHeightPx: Float,
-    bottomSheetHeight: Float,
-    sheetGesturesEnabled: Boolean,
-): Modifier =
-    nestedScrollFixedSwipeable(
-        state = state.wrapped,
-        anchors = mapOf(
-            fullHeight - peekHeightPx to BpkBottomSheetValue.Collapsed,
-            fullHeight - bottomSheetHeight to BpkBottomSheetValue.Expanded,
-        ),
-        orientation = Orientation.Vertical,
-        enabled = sheetGesturesEnabled,
-        resistance = null,
-    )
-
-private fun Modifier.bottomSheetSemantics(
-    state: BpkBottomSheetState,
-    peekHeightPx: Float,
-    bottomSheetHeight: Float,
-): Modifier = composed {
-    val scope = rememberCoroutineScope()
-    semantics {
-        if (peekHeightPx != bottomSheetHeight) {
-            if (state.isCollapsed) {
-                expand {
-                    if (state.confirmStateChange(BpkBottomSheetValue.Expanded)) {
-                        scope.launch { state.expand() }
-                    }
-                    true
-                }
-            } else {
-                collapse {
-                    if (state.confirmStateChange(BpkBottomSheetValue.Collapsed)) {
-                        scope.launch { state.collapse() }
-                    }
-                    true
-                }
-            }
-        }
-    }
-}
-
 @Composable
 private fun BpkBottomSheetHandle(
     modifier: Modifier = Modifier,
--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkBottomSheetState.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkBottomSheetState.kt
@@ -43,7 +43,7 @@ fun rememberBpkBottomSheetState(
             skipPartiallyExpanded = true,
             initialValue = initialValue.toBottomSheetValue(),
             confirmValueChange = { confirmStateChange(it.toBpkBottomSheetValue()) },
-            skipHiddenState = true,
+            skipHiddenState = false,
         )
     }
     return remember { BpkBottomSheetState(delegate) }
@@ -66,7 +66,7 @@ class BpkBottomSheetState internal constructor(
     ) : this(
         SheetState(
             skipPartiallyExpanded = true,
-            skipHiddenState = true,
+            skipHiddenState = false,
             initialValue = initialValue.toBottomSheetValue(),
             confirmValueChange = { confirmValueChange(it.toBpkBottomSheetValue()) },
         ),
