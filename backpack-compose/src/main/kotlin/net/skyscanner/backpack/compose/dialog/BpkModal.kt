37a0f31ffcaba818f5a45c7f47aa594b3dd2ac42
Hongrui Chen <hongruichen@skyscanner.net>
2023-08-30 18:52:13 +0800

    fix the dismiss animation

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/dialog/BpkModal.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/dialog/BpkModal.kt
@@ -19,6 +19,7 @@
 package net.skyscanner.backpack.compose.dialog
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
@@ -28,15 +29,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
-import kotlinx.coroutines.delay
 import net.skyscanner.backpack.compose.navigationbar.BpkTopNavBar
 import net.skyscanner.backpack.compose.navigationbar.NavIcon
 import net.skyscanner.backpack.compose.navigationbar.TextAction
@@ -53,52 +49,45 @@ fun BpkModal(
     onDismiss: (() -> Unit)? = null,
     content: @Composable BoxScope.() -> Unit,
 ) {
-    var visible by remember {
-        mutableStateOf(false)
+    val animVisibleState = remember {
+        MutableTransitionState(false).apply { targetState = true }
     }
 
-    var dismissed by remember {
-        mutableStateOf(false)
-    }
-
-    LaunchedEffect(dismissed) {
-        visible = !dismissed
-        if (dismissed) {
-            delay(ANIMATION_DURATION_MS.toLong())
-            onDismiss?.invoke()
-        }
+    if (!animVisibleState.targetState &&
+        !animVisibleState.currentState) {
+        onDismiss?.invoke()
+        return
     }
 
     Popup(
-        properties = PopupProperties(
-            focusable = true,
-            dismissOnBackPress = false,
-        ),
+        properties = PopupProperties(focusable = true),
+        onDismissRequest = { animVisibleState.targetState = false },
     ) {
         AnimatedVisibility(
-            visible = visible,
+            visibleState = animVisibleState,
             enter = slideInVertically(tween(ANIMATION_DURATION_MS)) { it },
             exit = slideOutVertically(tween(ANIMATION_DURATION_MS)) { it },
             modifier = Modifier.fillMaxSize(),
         ) {
-            Surface(
-                color = BpkTheme.colors.surfaceDefault,
-            ) {
+            Surface(color = BpkTheme.colors.surfaceDefault) {
                 Column {
                     if (action != null) {
                         BpkTopNavBar(
                             navIcon = NavIcon.Close(
                                 contentDescription = closeButtonAccessibilityLabel,
-                                onClick = { dismissed = true },
+                                onClick = { animVisibleState.targetState = false },
                             ),
                             title = title.orEmpty(),
-                            action = TextAction(text = action.text, onClick = { action.onClick.invoke() }),
+                            action = TextAction(text = action.text, onClick = {
+                                animVisibleState.targetState = false
+                                action.onClick.invoke()
+                            }),
                         )
                     } else {
                         BpkTopNavBar(
                             navIcon = NavIcon.Close(
                                 contentDescription = closeButtonAccessibilityLabel,
-                                onClick = { dismissed = true },
+                                onClick = { animVisibleState.targetState = false },
                             ),
                             title = title.orEmpty(),
                         )
