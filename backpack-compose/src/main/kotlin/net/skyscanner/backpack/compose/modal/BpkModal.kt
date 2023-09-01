9d0ef384d8d8ed6ffd254b70d7423880dac3cad7
hchen-skyscanner <143154763+hchen-skyscanner@users.noreply.github.com>
2023-09-02 00:00:31 +0800

    Add BpkModal component (#1711)
    * add BpkModal implementation
    
    * add animation
    
    * remove modifier
    
    * fix compiling error
    
    * rename & fix background color
    
    * fix the dismiss animation
    
    * save a surface
    
    * fix test for animation
    
    * move package: dialog -> modal
    
    * add readme and screenshots
    
    * move the action and icon outside the modal
    
    * fix remember state
    
    * reduce the cache use
    
    * LMN-401 Fix hide animation
    
    * make the `hide` as normal function
    
    * use back icon in action bar
    
    * wip
    
    * LMN-401 Tweak animation and show/hide functionality
    
    * LMN-401 Add back button demo
    
    * add ms in constant
    
    wip
    
    * update doc
    
    * update snapshot test
    
    * update doc
    
    * add license
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'dm'
    
    * Updated snapshots for 'default'
    
    * LMN-401 Update screenshots and docs
    
    * LMN-401 Simplify tests
    
    * LMN-401 Update content
    
    * LMN-401 Fix tests
    
    * Updated snapshots for 'rtl'
    
    * Updated snapshots for 'default'
    
    * Updated snapshots for 'dm'
    
    * Trigger CI
    
    ---------
    
    Co-authored-by: Maria Neumayer <maria.neumayer@skyscanner.net>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/modal/BpkModal.kt
@@ -0,0 +1,83 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.modal
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+import net.skyscanner.backpack.compose.navigationbar.BpkTopNavBar
+import net.skyscanner.backpack.compose.navigationbar.NavIcon
+import net.skyscanner.backpack.compose.navigationbar.TextAction
+import net.skyscanner.backpack.compose.theme.BpkTheme
+
+internal const val ModalAnimationDurationMs = 300
+
+@Composable
+@Suppress("ModifierMissing")
+fun BpkModal(
+    navIcon: NavIcon,
+    state: BpkModalState = rememberBpkModalState(),
+    action: TextAction? = null,
+    title: String? = null,
+    onDismiss: (() -> Unit)? = null,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    val isVisible = state.isVisible
+    if (isVisible.isIdle && !isVisible.currentState) {
+        onDismiss?.invoke()
+    }
+
+    Popup(
+        properties = PopupProperties(focusable = true),
+        onDismissRequest = { isVisible.targetState = false },
+    ) {
+        AnimatedVisibility(
+            visibleState = isVisible,
+            enter = slideInVertically(tween(ModalAnimationDurationMs)) { it },
+            exit = slideOutVertically(tween(ModalAnimationDurationMs)) { it },
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Column(modifier = Modifier.background(BpkTheme.colors.surfaceDefault)) {
+                if (action != null) {
+                    BpkTopNavBar(
+                        navIcon = navIcon,
+                        title = title.orEmpty(),
+                        action = action,
+                    )
+                } else {
+                    BpkTopNavBar(
+                        navIcon = navIcon,
+                        title = title.orEmpty(),
+                    )
+                }
+                Box(content = content)
+            }
+        }
+    }
+}
