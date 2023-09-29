0ae32d0eff36fec25e392897f7ef52ec8453af32
Maria Neumayer <maria.neumayer@skyscanner.net>
2023-09-29 17:38:31 +0100

    LMN-420 Remove custom semantics
    There was an issue where this was announcing various different slider options with percentage/value based content. This provided a confusing user behaviour.
    The current behaviour isn't perfect, but we'd need to build a completely custom slider to solve this. We can create an issue for the slider to improve. the API/accessibility support

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/slider/internal/BpkRangeSliderImpl.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/slider/internal/BpkRangeSliderImpl.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.progressSemantics
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.SliderDefaults
@@ -16,22 +15,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.disabled
 import androidx.compose.ui.semantics.invisibleToUser
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.setProgress
 import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.util.lerp
-import net.skyscanner.backpack.compose.R
 import net.skyscanner.backpack.compose.flare.BpkFlarePointerDirection
 import net.skyscanner.backpack.compose.text.BpkText
 import net.skyscanner.backpack.compose.theme.BpkTheme
 import net.skyscanner.backpack.compose.tokens.BpkBorderRadius
 import net.skyscanner.backpack.compose.tokens.BpkSpacing
 import net.skyscanner.backpack.compose.utils.FlareShape
-import kotlin.math.abs
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,12 +58,6 @@ internal fun BpkRangeSliderImpl(
                     label = it,
                     enabled = enabled,
                     interactionSource = startInteractionSource,
-                    value = value.start,
-                    valueRange = minValue..maxValue,
-                    steps = steps,
-                    sliderRangeType = stringResource(id = R.string.range_start),
-                    onValueChange = { changedVal -> onValueChange(changedVal..value.endInclusive) },
-                    onValueChangeFinished = onValueChangeFinished,
                 )
             } ?: SliderDefaults.Thumb(
                 interactionSource = startInteractionSource,
@@ -84,12 +71,6 @@ internal fun BpkRangeSliderImpl(
                     label = it,
                     enabled = enabled,
                     interactionSource = endInteractionSource,
-                    value = value.endInclusive,
-                    valueRange = minValue..maxValue,
-                    steps = steps,
-                    sliderRangeType = stringResource(id = R.string.range_end),
-                    onValueChange = { changedVal -> onValueChange(value.start..changedVal) },
-                    onValueChangeFinished = onValueChangeFinished,
                 )
             } ?: SliderDefaults.Thumb(
                 interactionSource = endInteractionSource,
@@ -104,28 +85,14 @@ internal fun BpkRangeSliderImpl(
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun SlideRangeLabel(
-    value: Float,
     enabled: Boolean,
     label: String,
-    onValueChange: (Float) -> Unit,
-    sliderRangeType: String,
     interactionSource: MutableInteractionSource,
-    valueRange: ClosedFloatingPointRange<Float>,
     modifier: Modifier = Modifier,
-    onValueChangeFinished: (() -> Unit)? = null,
-    steps: Int = 0,
 ) {
     Column(
         modifier = modifier
-            .sliderSemantics(
-                value = value,
-                enabled = enabled,
-                description = "$sliderRangeType, $label",
-                valueRange = valueRange,
-                steps = steps,
-                onValueChange = onValueChange,
-                onValueChangeFinished = onValueChangeFinished,
-            )
+            .semantics { stateDescription = label }
             .padding(bottom = BpkSpacing.Xl),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -169,49 +136,3 @@ internal fun sliderColors() = SliderDefaults.colors(
     activeTickColor = BpkTheme.colors.coreAccent,
     inactiveTickColor = BpkTheme.colors.line,
 )
-
-private fun Modifier.sliderSemantics(
-    value: Float,
-    enabled: Boolean,
-    description: String,
-    onValueChange: (Float) -> Unit,
-    onValueChangeFinished: (() -> Unit)? = null,
-    valueRange: ClosedFloatingPointRange<Float>,
-    steps: Int = 0,
-): Modifier {
-    val coerced = value.coerceIn(valueRange.start, valueRange.endInclusive)
-    return semantics {
-        if (!enabled) disabled()
-        this.stateDescription = description
-        setProgress(
-            action = { targetValue ->
-                var newValue = targetValue.coerceIn(valueRange.start, valueRange.endInclusive)
-                val originalVal = newValue
-                val resolvedValue = if (steps > 0) {
-                    var distance: Float = newValue
-                    for (i in 0..steps + 1) {
-                        val stepValue = lerp(
-                            valueRange.start,
-                            valueRange.endInclusive,
-                            i.toFloat() / (steps + 1),
-                        )
-                        if (abs(stepValue - originalVal) <= distance) {
-                            distance = abs(stepValue - originalVal)
-                            newValue = stepValue
-                        }
-                    }
-                    newValue
-                } else {
-                    newValue
-                }
-                if (resolvedValue == coerced) {
-                    false
-                } else {
-                    onValueChange(resolvedValue)
-                    onValueChangeFinished?.invoke()
-                    true
-                }
-            },
-        )
-    }.progressSemantics(value, valueRange, steps)
-}
