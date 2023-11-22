f5d8c20d64be9ca109bf207964f813179ede74ec
bvitaliyg <bvitaliyg@gmail.com>
2023-11-22 14:32:51 +0000

    Updated the generated files

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/tokens/internal/BpkInfoBannerColors.kt
@@ -0,0 +1,52 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Auto-generated: do not edit
+@file:Suppress("RedundantVisibilityModifier", "unused")
+
+package net.skyscanner.backpack.compose.tokens.`internal`
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import net.skyscanner.backpack.compose.utils.dynamicColorOf
+
+internal object BpkInfoBannerColors {
+    internal val default: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFEFF1F2), Color(0xFF131D2B))
+
+    internal val error: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFE70866), Color(0xFFFF649C))
+
+    internal val info: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFF545860), Color(0xFFBDC4CB))
+
+    internal val onContrast: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFFFFFFF), Color(0xFF131D2B))
+
+    internal val success: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFF0C838A), Color(0xFF62F1C6))
+
+    internal val warning: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFF55D42), Color(0xFFFEEB87))
+}
