aaad1e5752c5424371b44554166bb88fb340db8e
Jaana Inkinen <jaanainkinen@skyscanner.net>
2023-11-29 16:38:29 +0000

    Fix an incorrect selectionHint for return date that's before departure date

--- backpack-common/src/main/java/net/skyscanner/backpack/calendar2/data/CalendarCell.kt
+++ backpack-common/src/main/java/net/skyscanner/backpack/calendar2/data/CalendarCell.kt
@@ -184,7 +184,7 @@ private fun onClickLabel(
         is CalendarSelection.None -> selectionMode.startSelectionHint
         is CalendarSelection.Dates ->
             when {
-                selection.end != null -> selectionMode.startSelectionHint
+                selection.end != null || date < selection.start -> selectionMode.startSelectionHint
                 else -> selectionMode.endSelectionHint
             }
 
