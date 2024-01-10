80a968f020719fabb142215dea07f2103b1a36c4
Maria Neumayer <maria.neumayer@skyscanner.net>
2024-01-10 09:30:14 +0000

    LMN-843 Bump foundation

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/tokens/internal/BpkRatingBarColors.kt
@@ -0,0 +1,36 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Auto-generated: do not edit
+@file:Suppress("RedundantVisibilityModifier", "unused")
+
+package net.skyscanner.backpack.compose.tokens.`internal`
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import net.skyscanner.backpack.compose.utils.dynamicColorOf
+
+internal object BpkRatingBarColors {
+    internal val barTrackDefault: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFE0E4E9), Color(0xFF243346))
+
+    internal val barTrackOnContrast: Color
+        @Composable
+        get() = dynamicColorOf(Color(0xFFFFFFFF), Color(0xFF243346))
+}
