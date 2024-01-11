cc238fd512374f607560baec2256caf14408562a
Maria Neumayer <maria.neumayer@skyscanner.net>
2024-01-11 14:30:13 +0000

    Fix imports

--- app/src/test/java/net/skyscanner/backpack/meta/StoriesTests.kt
+++ app/src/test/java/net/skyscanner/backpack/meta/StoriesTests.kt
@@ -19,7 +19,7 @@
 package net.skyscanner.backpack.meta
 
 import net.skyscanner.backpack.demo.meta.StoriesRepository
-import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class StoriesTests {
@@ -28,21 +28,21 @@ class StoriesTests {
 
     @Test
     fun assertNoDemoOnlyStoriesInScreenshots() {
-        Assert.assertTrue(repository.screenshotStories.none { it.kind == StoryKind.DemoOnly })
+        assertTrue(repository.screenshotStories.none { it.kind == StoryKind.DemoOnly })
     }
 
     @Test
     fun assertNoScreenshotOnlyStoriesInDemos() {
-        Assert.assertTrue(repository.uiComponents.none {
+        assertTrue(repository.uiComponents.none {
             repository.storiesOf(it.name, compose = true).any { it.kind == StoryKind.ScreenshotOnly }
         })
-        Assert.assertTrue(repository.uiComponents.none {
+        assertTrue(repository.uiComponents.none {
             repository.storiesOf(it.name, compose = false).any { it.kind == StoryKind.ScreenshotOnly }
         })
-        Assert.assertTrue(repository.tokenComponents.none {
+        assertTrue(repository.tokenComponents.none {
             repository.storiesOf(it.name, compose = true).any { it.kind == StoryKind.ScreenshotOnly }
         })
-        Assert.assertTrue(repository.tokenComponents.none {
+        assertTrue(repository.tokenComponents.none {
             repository.storiesOf(it.name, compose = false).any { it.kind == StoryKind.ScreenshotOnly }
         })
     }
