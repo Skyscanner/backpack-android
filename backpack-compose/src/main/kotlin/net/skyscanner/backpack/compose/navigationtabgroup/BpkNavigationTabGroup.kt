a7c155f5336e178e91572b7935a029606b8e981b
Andra Georgescu <1880383+andra-georgescu@users.noreply.github.com>
2024-03-18 10:34:55 +0000

    New component - Navigation Tab Group (#1902)
    * added new navigation tab colours
    
    * Added new Navigation Tab Group component (compose only)
    
    * Added new story for Navigation Tab Group
    
    * Added snapshot tests for Navigation Tab Group
    
    * autogenerated README for new component
    
    * added new navigation tab colours
    
    * Added new Navigation Tab Group component (compose only)
    
    * Added new story for Navigation Tab Group
    
    * Added snapshot tests for Navigation Tab Group
    
    * autogenerated README for new component
    
    * fix UI details to match Figma
    
    * updated Readme and screenshots
    
    * Updated snapshots
    
    * Trigger CI
    
    * revert npm changes
    
    * add license heading
    
    ---------
    
    Co-authored-by: Andra-Georgescu <Andra.Georgescu@skyscanner.net>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/navigationtabgroup/BpkNavigationTabGroup.kt
@@ -0,0 +1,52 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.navigationtabgroup
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.icon.BpkIcon
+import net.skyscanner.backpack.compose.navigationtabgroup.internal.BpkNavigationTabGroupImpl
+
+data class BpkNavigationTabItem(val text: String, val icon: BpkIcon? = null)
+
+enum class BpkNavigationTabGroupStyle {
+    CanvasDefault,
+    SurfaceContrast,
+}
+
+@Composable
+fun BpkNavigationTabGroup(
+    tabs: List<BpkNavigationTabItem>,
+    selectedIndex: Int,
+    onItemClicked: (BpkNavigationTabItem) -> Unit,
+    modifier: Modifier = Modifier,
+    style: BpkNavigationTabGroupStyle = BpkNavigationTabGroupStyle.CanvasDefault,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    BpkNavigationTabGroupImpl(
+        tabs = tabs,
+        selectedIndex = selectedIndex,
+        onItemClicked = onItemClicked,
+        modifier = modifier,
+        style = style,
+        contentPadding = contentPadding,
+    )
+}
---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/navigationtabgroup/internal/BpkNavigationTabGroupImpl.kt
@@ -0,0 +1,84 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.navigationtabgroup.internal
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.navigationtabgroup.BpkNavigationTabGroupStyle
+import net.skyscanner.backpack.compose.navigationtabgroup.BpkNavigationTabItem
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
+
+@Composable
+internal fun BpkNavigationTabGroupImpl(
+    tabs: List<BpkNavigationTabItem>,
+    selectedIndex: Int,
+    onItemClicked: (BpkNavigationTabItem) -> Unit,
+    style: BpkNavigationTabGroupStyle,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    LazyRow(
+        modifier = modifier.selectableGroup(),
+        contentPadding = contentPadding,
+        state = rememberLazyListState(),
+        horizontalArrangement = Arrangement.spacedBy(BpkSpacing.Sm),
+    ) {
+        itemsIndexed(items = tabs) { index, tab ->
+            NavigationTabItem(
+                tab = tab,
+                selected = index == selectedIndex,
+                style = style,
+            ) {
+                onItemClicked.invoke(tabs[index])
+            }
+        }
+    }
+}
+
+@Composable
+private fun NavigationTabItem(
+    tab: BpkNavigationTabItem,
+    selected: Boolean,
+    style: BpkNavigationTabGroupStyle,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    val tabStyle = when (style) {
+        BpkNavigationTabGroupStyle.SurfaceContrast -> BpkNavigationTabStyle.SurfaceContrast
+        BpkNavigationTabGroupStyle.CanvasDefault -> BpkNavigationTabStyle.CanvasDefault
+    }
+    BpkNavigationTab(
+        modifier = modifier.semantics { role = Role.RadioButton },
+        text = tab.text,
+        icon = tab.icon,
+        selected = selected,
+        style = tabStyle,
+        onClick = onClick,
+    )
+}
