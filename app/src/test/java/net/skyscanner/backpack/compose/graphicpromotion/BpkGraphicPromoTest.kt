75c8a6503c6bc673ab09b9fcf87432f1036efc57
Gabriel Ferreira <gabriel.ferreira@skyscanner.net>
2024-10-11 16:17:14 +0200

    [MUON-542] - Update Readme, tests and screenshots

--- app/src/test/java/net/skyscanner/backpack/compose/graphicpromotion/BpkGraphicPromoTest.kt
+++ app/src/test/java/net/skyscanner/backpack/compose/graphicpromotion/BpkGraphicPromoTest.kt
@@ -21,11 +21,10 @@ package net.skyscanner.backpack.compose.graphicpromotion
 import net.skyscanner.backpack.compose.BpkSnapshotTest
 import net.skyscanner.backpack.compose.overlay.BpkOverlayType
 import net.skyscanner.backpack.demo.compose.BpkGraphicPromoSample
-import net.skyscanner.backpack.demo.compose.GraphicPromoStoryAlignmentBottomSponsored
 import net.skyscanner.backpack.demo.compose.GraphicPromoStoryAlignmentBottomWithText
-import net.skyscanner.backpack.demo.compose.GraphicPromoStoryAlignmentTopSponsored
 import net.skyscanner.backpack.demo.compose.GraphicPromoStoryAlignmentTopWithKicker
 import net.skyscanner.backpack.demo.compose.GraphicPromoStoryDefault
+import net.skyscanner.backpack.demo.compose.GraphicPromoStorySponsored
 import org.junit.Test
 
 class BpkGraphicPromoTest : BpkSnapshotTest() {
@@ -56,11 +55,6 @@ class BpkGraphicPromoTest : BpkSnapshotTest() {
     }
 
     @Test
-    fun topAlignedKickerSubHeadlineSponsoredAvailable() = snap {
-        GraphicPromoStoryAlignmentTopSponsored()
-    }
-
-    @Test
     fun bottomAlignedKickerAvailable() = snap {
         BpkGraphicPromoSample(
             subHeadline = "How to complete the climb in 3 days",
@@ -74,7 +68,12 @@ class BpkGraphicPromoTest : BpkSnapshotTest() {
     }
 
     @Test
-    fun bottomAlignedKickerSubHeadlineSponsoredAvailable() = snap {
-        GraphicPromoStoryAlignmentBottomSponsored()
+    fun defaultSponsoredAvailable() = snap {
+        GraphicPromoStorySponsored()
+    }
+
+    @Test
+    fun longTitleSponsoredAvailable() = snap {
+        GraphicPromoStorySponsored()
     }
 }
