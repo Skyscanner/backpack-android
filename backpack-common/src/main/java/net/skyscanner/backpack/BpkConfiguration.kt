bf5ec91c68305ac14f314dedf0be891e9511c581
Henrik Sym <henrik.sym@skyscanner.net>
2025-08-12 13:39:02 +0100

    DON-1741 Tidy up

--- backpack-common/src/main/java/net/skyscanner/backpack/BpkConfiguration.kt
+++ backpack-common/src/main/java/net/skyscanner/backpack/BpkConfiguration.kt
@@ -7,58 +7,44 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import net.skyscanner.backpack.common.R
 
-sealed class BpkExperimentalComponent {
-
-    data class Global(
-        @FontRes
-        val font: Int?,
-        @ColorRes
-        val colorResource: Int?,
-        val dimension: Dp?,
-        val radius: Int?,
-        @DimenRes
-        val dimensionResource: Int?,
-        val behaviour: Boolean?,
-    ) : BpkExperimentalComponent()
-
-    data class BpkButton(
-        @ColorRes
-        val colorResource: Int?,
-        val largeMinHeight: Dp?,
-        @DimenRes
-        val largeMinHeightResource: Int?,
-        val radius: Int?,
-        @DimenRes
-        val radiusDimension: Int?,
-    ) : BpkExperimentalComponent()
-
-    data class BpkText(
-        @FontRes
-        val font: Int?,
-    ) : BpkExperimentalComponent()
-
-    data class BpkCard(
-        val cornerRadius: Int?,
-    ) : BpkExperimentalComponent()
-
-    data class BpkChip(
-        @ColorRes
-        val colorResource: Int?,
-        val height: Dp?,
-        @DimenRes
-        val heightDimension: Int?,
-        val radius: Int?,
-        @DimenRes
-        val radiusDimension: Int?,
-    ) : BpkExperimentalComponent()
-}
-
 object BpkConfiguration {
+    sealed class BpkExperimentalComponent {
+
+        data class BpkButton(
+            @ColorRes
+            val colorResource: Int?,
+            val largeMinHeight: Dp?,
+            @DimenRes
+            val largeMinHeightResource: Int?,
+            val radius: Int?,
+            @DimenRes
+            val radiusDimension: Int?,
+        ) : BpkExperimentalComponent()
+
+        data class BpkText(
+            @FontRes
+            val font: Int?,
+        ) : BpkExperimentalComponent()
+
+        data class BpkCard(
+            val cornerRadius: Int?,
+        ) : BpkExperimentalComponent()
+
+        data class BpkChip(
+            @ColorRes
+            val colorResource: Int?,
+            val height: Dp?,
+            @DimenRes
+            val heightDimension: Int?,
+            val radius: Int?,
+            @DimenRes
+            val radiusDimension: Int?,
+        ) : BpkExperimentalComponent()
+    }
 
     private var _hasSet: Boolean = false
 
     fun setConfigs(
-        globalConfig: Boolean = false,
         chipConfig: Boolean = false,
         buttonConfig: Boolean = false,
         textConfig: Boolean = false,
@@ -68,8 +54,6 @@ object BpkConfiguration {
             throw IllegalStateException("BpkConfiguration has already been set")
         }
         _hasSet = true
-        if (globalConfig) {
-        }
         if (chipConfig) {
             this.chipConfig = BpkExperimentalComponent.BpkChip(
                 colorResource = R.color.bpkCoreAccent,
@@ -93,9 +77,6 @@ object BpkConfiguration {
         if (cardConfig) {
         }
     }
-
-    var globalConfig: BpkExperimentalComponent.Global? = null
-        private set
 
     var chipConfig: BpkExperimentalComponent.BpkChip? = null
         private set
