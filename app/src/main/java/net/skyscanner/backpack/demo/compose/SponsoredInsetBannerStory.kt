dee2bdde3f02ea133ed4e392029fece14270c35f
Joan Fuentes <joan.fuentes@skyscanner.net>
2025-10-17 11:29:11 +0200

    [MUON-1270] add new sponsored inset banner (#2455)
    * first unfinished version of BpkSponsoredInsetBanner
    
    * create story and improve BpkSponsoredInsetBanner.kt
    
    * add new SponsoredInsetBanner screenshots, documentation and tests
    
    * fix duplicated code and spelling
    
    * minor accessibility fixes
    
    * fix identation and imports
    
    * add tests
    
    * Updated snapshots
    
    ---------
    
    Co-authored-by: skyscanner-backpack-bot[bot] <197108191+skyscanner-backpack-bot[bot]@users.noreply.github.com>

---
+++ app/src/main/java/net/skyscanner/backpack/demo/compose/SponsoredInsetBannerStory.kt
@@ -0,0 +1,268 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 - 2025 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.demo.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.BiasAlignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerVariant.OnDark
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerVariant.OnLight
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBanner
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerCTA
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerVariant
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
+import net.skyscanner.backpack.demo.R
+import net.skyscanner.backpack.demo.components.InsetBannerComponent
+import net.skyscanner.backpack.demo.meta.ComposeStory
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnLight With Image")
+fun SponsoredInsetBannerStoryOnLightWithImage(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnLight,
+        title = stringResource(R.string.inset_banner_title),
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = getImage(),
+        logo = getPartnerLogo(variant = OnLight),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnLight without Image")
+fun SponsoredInsetBannerStoryOnLightWithoutImage(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnLight,
+        title = stringResource(R.string.inset_banner_title),
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnLight),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnLight Without subHeadline")
+fun SponsoredInsetBannerStoryOnLightWithoutSubHeadline(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnLight,
+        title = stringResource(R.string.inset_banner_title),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnLight),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnLight Without Title")
+fun SponsoredInsetBannerStoryOnLightWithoutTitle(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnLight,
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnLight),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnLight Without Title nor subHeadline")
+fun SponsoredInsetBannerStoryOnLightWithoutTitleNorSubHeadline(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnLight,
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnLight),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnDark With Image")
+fun SponsoredInsetBannerStoryOnDarkWithImage(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnDark,
+        title = stringResource(R.string.inset_banner_title),
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = getImage(),
+        logo = getPartnerLogo(variant = OnDark),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnDark Without Image")
+fun SponsoredInsetBannerStoryOnDarkWithoutImage(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnDark,
+        title = stringResource(R.string.inset_banner_title),
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnDark),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnDark Without subHeadline")
+fun SponsoredInsetBannerStoryOnDarkWithoutSubHeadline(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnDark,
+        title = stringResource(R.string.inset_banner_title),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnDark),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnDark Without Title")
+fun SponsoredInsetBannerStoryOnDarkWithoutTitle(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnDark,
+        subHeadline = stringResource(R.string.inset_banner_sub_headline),
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnDark),
+    )
+}
+
+@Composable
+@InsetBannerComponent
+@ComposeStory("Sponsored OnDark Without Title nor subHeadline")
+fun SponsoredInsetBannerStoryOnDarkWithoutTitleNorSubHeadline(modifier: Modifier = Modifier) {
+    DefaultSponsoredInsetBannerSample(
+        modifier = modifier,
+        variant = OnDark,
+        callToAction = BpkSponsoredInsetBannerCTA(
+            text = stringResource(R.string.inset_banner_cta_text),
+            accessibilityLabel = stringResource(R.string.inset_banner_cta_accessibility_label),
+            onClick = {},
+        ),
+        image = null,
+        logo = getPartnerLogo(variant = OnDark),
+    )
+}
+
+@Composable
+private fun getPartnerLogo(variant: BpkSponsoredInsetBannerVariant): @Composable () -> Unit = {
+    Image(
+        painter = when (variant) {
+            OnDark -> painterResource(R.drawable.inset_banner_skyland_white)
+            OnLight -> painterResource(R.drawable.inset_banner_skyland_black)
+        },
+        contentDescription = stringResource(R.string.inset_banner_logo_accessibility_label),
+        contentScale = ContentScale.Fit,
+    )
+}
+
+@Composable
+private fun getImage(): @Composable () -> Unit = {
+    Image(
+        painter = painterResource(R.drawable.city),
+        contentDescription = "",
+        contentScale = ContentScale.Crop,
+        alignment = BiasAlignment(horizontalBias = 0f, verticalBias = -0.6f),
+    )
+}
+
+@Composable
+internal fun DefaultSponsoredInsetBannerSample(
+    variant: BpkSponsoredInsetBannerVariant,
+    callToAction: BpkSponsoredInsetBannerCTA,
+    logo: @Composable (() -> Unit),
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    subHeadline: String? = null,
+    image: @Composable (() -> Unit)? = null,
+) {
+    Box(modifier = modifier.padding(BpkSpacing.Lg)) {
+        BpkSponsoredInsetBanner(
+            variant = variant,
+            backgroundColor = when (variant) {
+                OnDark -> androidx.compose.ui.graphics.Color(BACKGROUND_ONDARK_COLOR_HEX)
+                OnLight -> androidx.compose.ui.graphics.Color(BACKGROUND_ONLIGHT_COLOR_HEX)
+            },
+            title = title,
+            subHeadline = subHeadline,
+            callToAction = callToAction,
+            image = image,
+            logo = logo,
+        )
+    }
+}
---
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/insetbanner/internal/BpkSponsoredInsetBannerImpl.kt
@@ -0,0 +1,227 @@
+/**
+ * Backpack for Android - Skyscanner's Design System
+ *
+ * Copyright 2018 - 2025 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.skyscanner.backpack.compose.insetbanner.internal
+
+import android.annotation.SuppressLint
+import androidx.annotation.VisibleForTesting
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.unit.dp
+import net.skyscanner.backpack.compose.icon.BpkIcon
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerVariant
+import net.skyscanner.backpack.compose.insetbanner.BpkSponsoredInsetBannerCTA
+import net.skyscanner.backpack.compose.text.BpkText
+import net.skyscanner.backpack.compose.theme.BpkTheme
+import net.skyscanner.backpack.compose.tokens.BpkSpacing
+import net.skyscanner.backpack.compose.tokens.InformationCircle
+import net.skyscanner.backpack.compose.utils.clickableWithRipple
+import net.skyscanner.backpack.util.InternalBackpackApi
+
+@SuppressLint("RestrictedApi")
+@Composable
+internal fun BpkSponsoredInsetBannerImpl(
+    variant: BpkSponsoredInsetBannerVariant,
+    title: String?,
+    subHeadline: String?,
+    callToAction: BpkSponsoredInsetBannerCTA,
+    backgroundColor: Color,
+    logo: @Composable (() -> Unit),
+    image: @Composable (() -> Unit)?,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        BpkSponsoredInsetBannerHeader(
+            modifier = Modifier
+                .clip(
+                    shape = if (image != null) {
+                        RoundedCornerShape(
+                            topStart = BpkSpacing.Md,
+                            topEnd = BpkSpacing.Md,
+                            bottomStart = BpkSpacing.None,
+                            bottomEnd = BpkSpacing.None,
+                        )
+                    } else {
+                        RoundedCornerShape(BpkSpacing.Md)
+                    },
+                )
+                .background(
+                    color = backgroundColor,
+                ),
+            variant = variant,
+            title = title,
+            subHeadline = subHeadline,
+            callToAction = callToAction,
+            logo = logo,
+        )
+        image?.let {
+            ImageWithRoundedCornerShapeAtBottom(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IMAGE_HEIGHT)
+                    .testTag(SPONSORED_INSET_BANNER_IMAGE_TEST_TAG),
+                image = it,
+            )
+        }
+    }
+}
+
+@Composable
+internal fun ImageWithRoundedCornerShapeAtBottom(
+    image: @Composable (() -> Unit),
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .clip(
+                shape = RoundedCornerShape(
+                    topStart = BpkSpacing.None,
+                    topEnd = BpkSpacing.None,
+                    bottomStart = BpkSpacing.Md,
+                    bottomEnd = BpkSpacing.Md,
+                ),
+            ),
+    ) {
+        image()
+    }
+}
+
+@Composable
+internal fun BpkSponsoredInsetBannerHeader(
+    variant: BpkSponsoredInsetBannerVariant,
+    callToAction: BpkSponsoredInsetBannerCTA,
+    modifier: Modifier = Modifier,
+    title: String? = null,
+    subHeadline: String? = null,
+    logo: @Composable (() -> Unit),
+) {
+    Column(modifier = modifier.fillMaxWidth().padding(bottom = BpkSpacing.Md)) {
+        Row(
+            modifier = Modifier.fillMaxWidth().heightIn(max = LOGO_HEIGHT + BpkSpacing.Md),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(top = BpkSpacing.Md, start = BpkSpacing.Base)
+                    .widthIn(max = LOGO_WIDTH)
+                    .heightIn(max = LOGO_HEIGHT),
+            ) {
+                logo()
+            }
+            SponsorCTASection(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .clickableWithRipple { callToAction.onClick() }
+                    .padding(top = BpkSpacing.Md, start = BpkSpacing.Base, end = BpkSpacing.Base),
+                callToAction = callToAction,
+                variant = variant,
+            )
+        }
+        if (!title.isNullOrBlank()) {
+            BpkText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = BpkSpacing.Base, end = BpkSpacing.Base, top = BpkSpacing.Sm),
+                text = title,
+                style = BpkTheme.typography.label2,
+                color = getTextColor(variant = variant),
+            )
+        }
+        if (!subHeadline.isNullOrBlank()) {
+            val topPadding = if (title.isNullOrBlank()) BpkSpacing.Sm else 0.dp
+            BpkText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = BpkSpacing.Base, end = BpkSpacing.Base, top = topPadding),
+                text = subHeadline,
+                style = BpkTheme.typography.caption,
+                color = getTextColor(variant = variant),
+            )
+        }
+    }
+}
+
+@Composable
+private fun SponsorCTASection(
+    callToAction: BpkSponsoredInsetBannerCTA,
+    variant: BpkSponsoredInsetBannerVariant,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clearAndSetSemantics {
+                this.contentDescription = callToAction.accessibilityLabel
+                role = Role.Button
+            },
+        horizontalArrangement = Arrangement.spacedBy(space = BpkSpacing.Md),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        BpkText(
+            text = callToAction.text,
+            style = BpkTheme.typography.caption,
+            color = getTextColor(variant = variant),
+        )
+        BpkIcon(
+            icon = BpkIcon.InformationCircle,
+            contentDescription = null,
+            tint = getTintColor(variant = variant),
+        )
+    }
+}
+
+private val LOGO_WIDTH = 88.dp
+private val LOGO_HEIGHT = 22.dp
+private val IMAGE_HEIGHT = 120.dp
+
+@Composable
+private fun getTextColor(variant: BpkSponsoredInsetBannerVariant): Color =
+    when (variant) {
+        BpkSponsoredInsetBannerVariant.OnLight -> BpkTheme.colors.textOnLight
+        BpkSponsoredInsetBannerVariant.OnDark -> BpkTheme.colors.textOnDark
+    }
+
+@Composable
+private fun getTintColor(variant: BpkSponsoredInsetBannerVariant): Color =
+    when (variant) {
+        BpkSponsoredInsetBannerVariant.OnLight -> BpkTheme.colors.textOnLight
+        BpkSponsoredInsetBannerVariant.OnDark -> BpkTheme.colors.textOnDark
+    }
+
+@VisibleForTesting(otherwise = VisibleForTesting.NONE)
+@InternalBackpackApi
+const val SPONSORED_INSET_BANNER_IMAGE_TEST_TAG = "SponsoredInsetBannerImageTestTag"
