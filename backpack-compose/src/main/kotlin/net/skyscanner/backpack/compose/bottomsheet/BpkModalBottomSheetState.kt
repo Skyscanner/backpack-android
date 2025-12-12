90ff3dc4bf176e56ac0a95f13c2a797062cdc1b0
Henrik Sym <141921186+henrik-sky@users.noreply.github.com>
2025-12-12 16:35:36 +0000

    DON-2146 Replace the Compose compiler workaround for confirmStateChange (#2519)

--- backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkModalBottomSheetState.kt
+++ backpack-compose/src/main/kotlin/net/skyscanner/backpack/compose/bottomsheet/BpkModalBottomSheetState.kt
@@ -30,9 +30,7 @@ import androidx.compose.runtime.remember
 @OptIn(ExperimentalMaterial3Api::class)
 fun rememberBpkModalBottomSheetState(
     skipPartiallyExpanded: Boolean = false,
-    // workaround for: https://issuetracker.google.com/issues/342089957 - remove with next compose compiler update
-//    confirmStateChange: (BpkModalBottomSheetValue) -> Boolean = { true },
-    confirmStateChange: (BpkModalBottomSheetValue) -> Boolean = remember { { true } },
+    confirmStateChange: (BpkModalBottomSheetValue) -> Boolean = { true },
 ): BpkModalBottomSheetState {
     val delegate = rememberModalBottomSheetState(
         skipPartiallyExpanded = skipPartiallyExpanded,
