324ea099c1ee559102511b0b3776362872d092ac
lokmane.krizou@skyscanner.net <lokmane.krizou@skyscanner.net>
2026-01-30 07:47:44 +0100

    address comment

--- backpack-lint/src/main/java/net/skyscanner/backpack/lint/check/UnicodeIconUsageDetector.kt
+++ backpack-lint/src/main/java/net/skyscanner/backpack/lint/check/UnicodeIconUsageDetector.kt
@@ -36,7 +36,7 @@ import org.jetbrains.uast.UPolyadicExpression
 class UnicodeIconUsageDetector : Detector(), SourceCodeScanner {
 
     companion object {
-        private val EXPLANATION =
+        private const val EXPLANATION =
             "Unicode symbols used as icons are not accessible. Use BpkIcon composables with contentDescription instead.\n\n" +
                 LintConstants.SUPPORT_MESSAGE
 
